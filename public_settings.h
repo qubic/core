84d0ebac9e407751ac75f5272a9f6096a9c2260d
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2023-11-03 16:55:28 +0100

    move several parts to header files

---
+++ public_settings.h
@@ -0,0 +1,29 @@
+#pragma once
+
+////////// Public Settings \\\\\\\\\\
+
+#define AVX512 0
+
+#define VERSION_A 1
+#define VERSION_B 178
+#define VERSION_C 0
+
+#define EPOCH 81
+#define TICK 10500000
+
+#define ARBITRATOR "AFZPUAIYVPNUYGJRQVLUKOPPVLHAZQTGLYAAUUNBXFTVTAMSBKQBLEIEPCVJ"
+
+#define IGNORE_RESOURCE_TESTING 1
+
+static unsigned short SYSTEM_FILE_NAME[] = L"system";
+static unsigned short SPECTRUM_FILE_NAME[] = L"spectrum.???";
+static unsigned short UNIVERSE_FILE_NAME[] = L"universe.???";
+static unsigned short CONTRACT_FILE_NAME[] = L"contract????.???";
+
+#define DATA_LENGTH 2000
+#define INFO_LENGTH 1000
+#define NUMBER_OF_INPUT_NEURONS 1000
+#define NUMBER_OF_OUTPUT_NEURONS 1000
+#define MAX_INPUT_DURATION 200
+#define MAX_OUTPUT_DURATION 200
+#define SOLUTION_THRESHOLD 1116
