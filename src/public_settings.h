5a4d0ec23c55361b32e0f4440690f0a786c4ad96
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2023-11-21 15:36:12 +0100

    prepare for release (ep84, v1.181)

--- src/public_settings.h
+++ src/public_settings.h
@@ -6,11 +6,11 @@
 #define MAX_NUMBER_OF_PROCESSORS 32
 
 #define VERSION_A 1
-#define VERSION_B 180
+#define VERSION_B 181
 #define VERSION_C 0
 
-#define EPOCH 83
-#define TICK 10800000
+#define EPOCH 84
+#define TICK 10900000
 
 #define RANDOM_SEED0 10
 #define RANDOM_SEED1 9
@@ -31,12 +31,12 @@ static unsigned short UNIVERSE_FILE_NAME[] = L"universe.???";
 static unsigned short SCORE_CACHE_FILE_NAME[] = L"score.???";
 static unsigned short CONTRACT_FILE_NAME[] = L"contract????.???";
 
-#define DATA_LENGTH 2000
+#define DATA_LENGTH 1000
 #define INFO_LENGTH 1000
 #define NUMBER_OF_INPUT_NEURONS 1000
 #define NUMBER_OF_OUTPUT_NEURONS 1000
 #define MAX_INPUT_DURATION 200
 #define MAX_OUTPUT_DURATION 200
-#define SOLUTION_THRESHOLD 1114
+#define SOLUTION_THRESHOLD 583
 #define USE_SCORE_CACHE 1
 #define SCORE_CACHE_SIZE 1000000 // the larger the better
