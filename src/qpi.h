4e68c38068f7615a31f072e77579c84323dc0d8a
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2024-01-08 11:45:20 +0100

    qpi.h: add pragma once, docs, fix typo

--- src/qpi.h
+++ src/qpi.h
@@ -1,5 +1,7 @@
 // Qubic Programming Interface 1.0.0
 
+#pragma once
+
 #include "platform/m256.h"
 
 namespace QPI
@@ -20,6 +22,8 @@ namespace QPI
 	]
 	__
 	union
+	float
+	double
 
 	*/
 
@@ -5340,7 +5344,7 @@ namespace QPI
 
 	#define LOG_WARNING(message) __logContractWarningMessage(message);
 
-	#define PRIVATE(functionOrProcedure) private: static void functionOrProcedure(CONTRACT_STATE_TYPE& statetate, functionOrProcedure##_input& input, functionOrProcedure##_output& output) { constexpr unsigned int __functionOrProcedureId = (CONTRACT_INDEX << 22) | __LINE__; ::__beginFunctionOrProcedure(__functionOrProcedureId);
+	#define PRIVATE(functionOrProcedure) private: static void functionOrProcedure(CONTRACT_STATE_TYPE& state, functionOrProcedure##_input& input, functionOrProcedure##_output& output) { constexpr unsigned int __functionOrProcedureId = (CONTRACT_INDEX << 22) | __LINE__; ::__beginFunctionOrProcedure(__functionOrProcedureId);
 
 	#define PUBLIC(functionOrProcedure) public: static void functionOrProcedure(CONTRACT_STATE_TYPE& state, functionOrProcedure##_input& input, functionOrProcedure##_output& output) { constexpr unsigned int __functionOrProcedureId = (CONTRACT_INDEX << 22) | __LINE__; ::__beginFunctionOrProcedure(__functionOrProcedureId);
 
