e949ed276f84b2710e89b934bd3d25431ab088b0
krypdkat <cryptodkat@gmail.com>
2024-02-07 13:39:02 +0700

    testnet configs

--- src/private_settings.h
+++ src/private_settings.h
@@ -4,7 +4,7 @@
 
 // Do NOT share the data of "Private Settings" section with anybody!!!
 
-#define OPERATOR "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+#define OPERATOR "MEFKYFCDXDUILCAJKOIKWQAPENJDUHSSYPBRWFOTLALILAYWQFDSITJELLHG"
 
 static unsigned char computorSeeds[][55 + 1] = {
     "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
@@ -23,19 +23,20 @@ static const unsigned char whiteListPeers[][4] = {
 };
 
 #define LOG_BUFFER_SIZE 16777200 // Must be less or equal to 16777200
-#define LOG_QU_TRANSFERS 0 // "0" disables logging, "1" enables it
-#define LOG_QU_TRANSFERS_TRACK_TRANSFER_ID 0
-#define LOG_ASSET_ISSUANCES 0
-#define LOG_ASSET_OWNERSHIP_CHANGES 0
-#define LOG_ASSET_POSSESSION_CHANGES 0
-#define LOG_CONTRACT_ERROR_MESSAGES 0
-#define LOG_CONTRACT_WARNING_MESSAGES 0
-#define LOG_CONTRACT_INFO_MESSAGES 0
-#define LOG_CONTRACT_DEBUG_MESSAGES 0
-#define LOG_BURNINGS 0
-#define LOG_CUSTOM_MESSAGES 0
+#define LOG_QU_TRANSFERS 1 // "0" disables logging, "1" enables it
+#define LOG_QU_TRANSFERS_TRACK_TRANSFER_ID 1
+#define LOG_ASSET_ISSUANCES 1
+#define LOG_ASSET_OWNERSHIP_CHANGES 1
+#define LOG_ASSET_POSSESSION_CHANGES 1
+#define LOG_CONTRACT_ERROR_MESSAGES 1
+#define LOG_CONTRACT_WARNING_MESSAGES 1
+#define LOG_CONTRACT_INFO_MESSAGES 1
+#define LOG_CONTRACT_DEBUG_MESSAGES 1
+#define LOG_BURNINGS 1
+#define LOG_CUSTOM_MESSAGES 1
 static unsigned long long logReaderPasscodes[][4] = {
-    {0, 0, 0, 0}, // REMOVE THIS ENTRY AND REPLACE IT WITH YOUR OWN RANDOM NUMBERS IN [0..18446744073709551615] RANGE IF LOGGING IS ENABLED
+    {1, 2, 3, 4}, // change the passcode if needed
+    {5, 6, 7, 8}, 
 };
 
 // Mode for auto save ticks:
@@ -48,4 +49,4 @@ static unsigned long long logReaderPasscodes[][4] = {
 // Perform state persisting when your node is misaligned will also make your node misaligned after resuming.
 // Thus, picking various TICK_STORAGE_AUTOSAVE_TICK_PERIOD numbers across AUX nodes is recommended.
 // some suggested prime numbers you can try: 971 977 983 991 997
-#define TICK_STORAGE_AUTOSAVE_TICK_PERIOD 1000
+#define TICK_STORAGE_AUTOSAVE_TICK_PERIOD 100
