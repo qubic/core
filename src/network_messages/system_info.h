3f45da43a996592a64f40bfe5e726d6f9a5da5c1
J0ET0M <107187448+J0ET0M@users.noreply.github.com>
2024-02-11 13:20:22 +0000

    Feature/2024 02 09 system info network message (#52)
    * added system info request/response
    
    * moved system_info.h to folder
    
    * remove unrelated check
    
    (3 commits squashed)
    
    ---------
    
    Co-authored-by: Philipp Werner <22914157+philippwerner@users.noreply.github.com>

---
+++ src/network_messages/system_info.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "common_def.h"
+
+#define REQUEST_SYSTEM_INFO 46
+
+
+#define RESPOND_SYSTEM_INFO 47
+
+struct RespondSystemInfo
+{
+    short version;
+    unsigned short epoch;
+    unsigned int tick;
+    unsigned int initialTick;
+    unsigned int latestCreatedTick;
+
+    unsigned short initialMillisecond;
+    unsigned char initialSecond;
+    unsigned char initialMinute;
+    unsigned char initialHour;
+    unsigned char initialDay;
+    unsigned char initialMonth;
+    unsigned char initialYear;
+
+    unsigned int numberOfEntities;
+    unsigned int numberOfTransactions;
+
+    m256i randomMiningSeed;
+};
+
+static_assert(sizeof(RespondSystemInfo) == 2 + 2 + 4 + 4 + 4 + 2 + 1 + 1 + 1 + 1 + 1 + 1 + 4 + 32 + 4, "Something is wrong with the struct size of RespondSystemInfo.");
