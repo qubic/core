a240bd3e68784163575ed42ee709f2f04f0d1e4c
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2024-02-14 16:00:52 +0100

    Reset another array before beginning of epoch

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -2423,6 +2423,11 @@ static void beginEpoch1of2()
     bs->SetMem(tickTransactions, FIRST_TICK_TRANSACTION_OFFSET + (((unsigned long long)MAX_NUMBER_OF_TICKS_PER_EPOCH) * NUMBER_OF_TRANSACTIONS_PER_TICK * MAX_TRANSACTION_SIZE / TRANSACTION_SPARSENESS), 0);
     bs->SetMem(tickTransactionOffsets, sizeof(tickTransactionOffsets), 0);
 
+    for (unsigned int i = 0; i < SPECTRUM_CAPACITY; i++)
+    {
+        ((Transaction*)&entityPendingTransactions[i * MAX_TRANSACTION_SIZE])->tick = 0;
+    }
+
     bs->SetMem(solutionPublicationTicks, sizeof(solutionPublicationTicks), 0);
     bs->SetMem(faultyComputorFlags, sizeof(faultyComputorFlags), 0);
 
@@ -3432,10 +3437,6 @@ static bool initialize()
             logStatusToConsole(L"EFI_BOOT_SERVICES.AllocatePool() fails", status, __LINE__);
 
             return false;
-        }
-        for (unsigned int i = 0; i < SPECTRUM_CAPACITY; i++)
-        {
-            ((Transaction*)&entityPendingTransactions[i * MAX_TRANSACTION_SIZE])->tick = 0;
         }
 
         if (status = bs->AllocatePool(EfiRuntimeServicesData, SPECTRUM_CAPACITY * sizeof(::Entity) >= ASSETS_CAPACITY * sizeof(Asset) ? SPECTRUM_CAPACITY * sizeof(::Entity) : ASSETS_CAPACITY * sizeof(Asset), (void**)&reorgBuffer))
