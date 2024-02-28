95b24d74368a32d24d721ba6880191d6a871fcbb
krypdkat <cryptodkat@gmail.com>
2024-02-28 19:33:49 +0700

    not accepting null id as computor

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -576,6 +576,18 @@ static void processBroadcastMessage(const unsigned long long processorNumber, Re
 static void processBroadcastComputors(Peer* peer, RequestResponseHeader* header)
 {
     BroadcastComputors* request = header->getPayload<BroadcastComputors>();
+    // verify that all address are non-zeroes
+    // discard it even if ARB broadcast it
+    if (request->computors.epoch > broadcastedComputors.broadcastComputors.computors.epoch)
+    {
+        for (unsigned int i = 0; i < NUMBER_OF_COMPUTORS; i++)
+        {
+            if (request->computors.publicKeys[i] == NULL_ID) // NULL_ID is _mm256_setzero_si256()
+            {
+                return;
+            }
+        }
+    }
     if (request->computors.epoch > broadcastedComputors.broadcastComputors.computors.epoch)
     {
         unsigned char digest[32];
@@ -3567,7 +3579,6 @@ static bool initialize()
         }
 
         bs->SetMem(solutionThreshold, sizeof(int) * MAX_NUMBER_EPOCH, 0);
-
         if (status = bs->AllocatePool(EfiRuntimeServicesData, NUMBER_OF_MINER_SOLUTION_FLAGS / 8, (void**)&minerSolutionFlags))
         {
             logStatusToConsole(L"EFI_BOOT_SERVICES.AllocatePool() fails", status, __LINE__);
@@ -4718,6 +4729,7 @@ EFI_STATUS efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE* systemTable)
                 {
                     saveSystem();
                     systemMustBeSaved = false;
+                    systemDataSavingTick = curTimeTick;
                 }
                 if (spectrumMustBeSaved)
                 {
