18c715afc436b503c8d3715eef69ec5efccf8868
krypdkat <cryptodkat@gmail.com>
2024-03-21 22:03:46 +0700

    add log when pressing key

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -4561,6 +4561,7 @@ static void processKeyPresses()
         */
         case 0x0E:
         {
+            logToConsole("Pressed F4 key");
             for (unsigned int i = 0; i < NUMBER_OF_OUTGOING_CONNECTIONS + NUMBER_OF_INCOMING_CONNECTIONS; i++)
             {
                 closePeer(&peers[i]);
@@ -4575,6 +4576,7 @@ static void processKeyPresses()
         */
         case 0x0F:
         {
+            logToConsole("Pressed F5 key");
             forceNextTick = true;
         }
         break;
@@ -4586,6 +4588,7 @@ static void processKeyPresses()
         */
         case 0x10:
         {
+            logToConsole("Pressed F6 key");
             SPECTRUM_FILE_NAME[sizeof(SPECTRUM_FILE_NAME) / sizeof(SPECTRUM_FILE_NAME[0]) - 4] = L'0';
             SPECTRUM_FILE_NAME[sizeof(SPECTRUM_FILE_NAME) / sizeof(SPECTRUM_FILE_NAME[0]) - 3] = L'0';
             SPECTRUM_FILE_NAME[sizeof(SPECTRUM_FILE_NAME) / sizeof(SPECTRUM_FILE_NAME[0]) - 2] = L'0';
@@ -4611,6 +4614,7 @@ static void processKeyPresses()
         */
         case 0x11:
         {
+            logToConsole("Pressed F7 key");
             forceSwitchEpoch = true;
         }
         break;
@@ -4622,6 +4626,7 @@ static void processKeyPresses()
         */
         case 0x13:
         {
+            logToConsole("Pressed F9 key");
             if (system.latestCreatedTick > 0) system.latestCreatedTick--;
         }
         break;
