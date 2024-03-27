829bddc8005e9299507ef7dc18fc231d62699dc7
krypdkat <cryptodkat@gmail.com>
2024-03-27 09:20:23 +0700

    increase solution threshold

--- src/public_settings.h
+++ src/public_settings.h
@@ -52,7 +52,7 @@ static unsigned short CONTRACT_FILE_NAME[] = L"contract????.???";
 #define MAX_INPUT_DURATION 256
 #define MAX_OUTPUT_DURATION 256
 #define NEURON_VALUE_LIMIT 1LL
-#define SOLUTION_THRESHOLD_DEFAULT 42
+#define SOLUTION_THRESHOLD_DEFAULT 43
 
 // include commonly needed definitions
 #include "network_messages/common_def.h"
