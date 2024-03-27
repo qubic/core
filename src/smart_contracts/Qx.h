404ad9901df66181feebb097868e901d0d54cca8
CFB-QUBIC <cfb@qubic.li>
2024-03-27 14:40:35 +0300

    Modified QX::TransferShareOwnershipAndPossession() to account for reserved shares.

--- src/smart_contracts/Qx.h
+++ src/smart_contracts/Qx.h
@@ -170,6 +170,52 @@ private:
 	uint32 _transferFee; // Amount of qus
 	uint32 _tradeFee; // Number of billionths
 
+	struct _AssetOrder
+	{
+		id entity;
+		sint64 numberOfShares;
+	};
+	collection<_AssetOrder, 8388608> _assetOrders;
+
+	struct _EntityOrder
+	{
+		id issuer;
+		uint64 assetName;
+		sint64 numberOfShares;
+	};
+	collection<_EntityOrder, 8388608> _entityOrders;
+
+	sint64 _elementIndex;
+	_EntityOrder _entityOrder;
+
+	struct _NumberOfReservedShares_input
+	{
+		id issuer;
+		id ownerAndPossessor;
+		uint64 assetName;
+	} _numberOfReservedShares_input;
+	struct _NumberOfReservedShares_output
+	{
+		sint64 numberOfShares;
+	} _numberOfReservedShares_output;
+
+	PRIVATE(_NumberOfReservedShares)
+
+		output.numberOfShares = 0;
+
+		state._elementIndex = state._entityOrders.headIndex(input.ownerAndPossessor, 0);
+		while (state._elementIndex != NULL_INDEX)
+		{
+			state._entityOrder = state._entityOrders.element(state._elementIndex);
+			if (state._entityOrder.assetName == input.assetName
+				&& state._entityOrder.issuer == input.issuer)
+			{
+				output.numberOfShares += state._entityOrder.numberOfShares;
+			}
+			state._elementIndex = state._entityOrders.nextElementIndex(state._elementIndex);
+		}
+	_
+
 	PUBLIC(Fees)
 
 		output.assetIssuanceFee = state._assetIssuanceFee;
@@ -231,7 +277,18 @@ private:
 			}
 			state._earnedAmount += state._transferFee;
 
-			output.transferredNumberOfShares = transferShareOwnershipAndPossession(input.assetName, input.issuer, invocator(), invocator(), input.numberOfShares, input.newOwnerAndPossessor) < 0 ? 0 : input.numberOfShares;
+			state._numberOfReservedShares_input.issuer = input.issuer;
+			state._numberOfReservedShares_input.ownerAndPossessor = invocator();
+			state._numberOfReservedShares_input.assetName = input.assetName;
+			_NumberOfReservedShares(state, state._numberOfReservedShares_input, state._numberOfReservedShares_output);
+			if (numberOfPossessedShares(input.assetName, input.issuer, invocator(), invocator(), CONTRACT_INDEX, CONTRACT_INDEX) - state._numberOfReservedShares_output.numberOfShares < input.numberOfShares)
+			{
+				output.transferredNumberOfShares = 0;
+			}
+			else
+			{
+				output.transferredNumberOfShares = transferShareOwnershipAndPossession(input.assetName, input.issuer, invocator(), invocator(), input.numberOfShares, input.newOwnerAndPossessor) < 0 ? 0 : input.numberOfShares;
+			}
 		}
 	_
 
