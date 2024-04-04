f06b4fa784aa74c5dfa83a1d3a6fc6b87f482d77
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2024-04-04 12:29:43 +0200

    Correctly update computer digest when state of contract 0 is changed (#84)
    * When contract 0 state is changed, set flag for updating digest
    
    * Wrap fee reserve change into function

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -1434,6 +1434,13 @@ static void requestProcessor(void* ProcedureArgument)
     }
 }
 
+// Return reference to fee reserve of contract for changing its value (data stored in state of contract 0)
+static long long & contractFeeReserve(unsigned int contractIndex)
+{
+    contractStateChangeFlags[0] |= 1ULL;
+    return ((Contract0State*)contractStates[0])->contractFeeReserves[contractIndex];
+}
+
 static void __beginFunctionOrProcedure(const unsigned int functionOrProcedureId)
 {
     // TODO
@@ -1486,8 +1493,7 @@ static long long __burn(long long amount)
 
     if (decreaseEnergy(index, amount))
     {
-        Contract0State* contract0State = (Contract0State*)contractStates[0];
-        contract0State->contractFeeReserves[executedContractIndex] += amount;
+        contractFeeReserve(executedContractIndex) += amount;
 
         const Burning burning = { currentContract , amount };
         logBurning(burning);
@@ -2756,7 +2762,6 @@ static void endEpoch()
     getUniverseDigest(etalonTick.prevUniverseDigest);
     getComputerDigest(etalonTick.prevComputerDigest);
 
-    Contract0State* contract0State = (Contract0State*)contractStates[0];
     for (unsigned int contractIndex = 1; contractIndex < sizeof(contractDescriptions) / sizeof(contractDescriptions[0]); contractIndex++)
     {
         if (system.epoch < contractDescriptions[contractIndex].constructionEpoch)
@@ -2807,7 +2812,7 @@ static void endEpoch()
                 logQuTransfer(quTransfer);
             }
 
-            contract0State->contractFeeReserves[contractIndex] = finalPrice * NUMBER_OF_COMPUTORS;
+            contractFeeReserve(contractIndex) = finalPrice * NUMBER_OF_COMPUTORS;
         }
     }
 
