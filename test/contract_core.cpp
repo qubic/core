4089af3a2210819314a6996fe4f3b728e2d30669
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2024-04-15 16:39:04 +0200

    Add stack buffer + test

---
+++ test/contract_core.cpp
@@ -0,0 +1,47 @@
+#define NO_UEFI
+
+#include "gtest/gtest.h"
+
+#define TRACK_MAX_STACK_BUFFER_SIZE
+#include "../src/contract_core/stack_buffer.h"
+
+TEST(TestCoreContractCore, StackBuffer)
+{
+    StackBuffer<unsigned char, 120> s1;
+    s1.init();
+    EXPECT_EQ(s1.capacity(), 120);
+    EXPECT_EQ(s1.size(), 0);
+    EXPECT_EQ(s1.maxSizeObserved(), 0);
+
+    EXPECT_NE(s1.allocate(70), nullptr);    // success
+    EXPECT_EQ(s1.allocate(50), nullptr);    // fail
+    EXPECT_EQ(s1.allocate(100), nullptr);   // fail
+    EXPECT_EQ(s1.allocate(255), nullptr);   // fail
+    EXPECT_EQ(s1.size(), 71);
+    s1.free();
+    EXPECT_EQ(s1.size(), 0);
+    EXPECT_EQ(s1.maxSizeObserved(), 71);
+
+    EXPECT_NE(s1.allocate(10), nullptr);    // success
+        EXPECT_NE(s1.allocate(20), nullptr);    // success
+            EXPECT_NE(s1.allocate(30), nullptr);    // success
+                EXPECT_NE(s1.allocate(40), nullptr);    // success
+                    EXPECT_EQ(s1.size(), 104);
+                s1.free();
+                EXPECT_EQ(s1.size(), 63);
+                EXPECT_NE(s1.allocate(20), nullptr);    // success
+                    EXPECT_EQ(s1.size(), 84);
+                    EXPECT_EQ(s1.allocate(50), nullptr);    // fail
+                    EXPECT_EQ(s1.allocate(100), nullptr);   // fail
+                    EXPECT_EQ(s1.allocate(255), nullptr);   // fail
+                s1.free();
+                EXPECT_EQ(s1.size(), 63);
+            s1.free();
+            EXPECT_EQ(s1.size(), 32);
+        s1.free();
+        EXPECT_EQ(s1.size(), 11);
+    s1.free();
+    EXPECT_EQ(s1.size(), 0);
+
+    
+}
