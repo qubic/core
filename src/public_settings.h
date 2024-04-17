e6b2d74d78f5660c18f2d68e8c19e751fb0d94df
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2024-04-17 14:29:58 +0200

    Update params for epoch 105 (start node after seamless transition)

--- src/public_settings.h
+++ src/public_settings.h
@@ -24,7 +24,7 @@
 // Set START_NETWORK_FROM_SCRATCH to 0 if you start the node for syncing with the already ticking network.
 // If this flag is 1, it indicates that the whole network (all 676 IDs) will start from scratch and agree that the very first tick time will be set at (2022-04-13 Wed 12:00:00.000UTC).
 // If this flag is 0, the node will try to fetch the initial tick of the epoch from other nodes, because the tick's timestamp may differ from (2022-04-13 Wed 12:00:00.000UTC).
-#define START_NETWORK_FROM_SCRATCH 1
+#define START_NETWORK_FROM_SCRATCH 0
 
 //////////////////////////////////////////////////////////////////////////
 // Config options that should NOT be changed by operators
@@ -33,8 +33,8 @@
 #define VERSION_B 199
 #define VERSION_C 0
 
-#define EPOCH 104
-#define TICK 13360000
+#define EPOCH 105
+#define TICK 13461049
 
 #define ARBITRATOR "AFZPUAIYVPNUYGJRQVLUKOPPVLHAZQTGLYAAUUNBXFTVTAMSBKQBLEIEPCVJ"
 
