2a350a0351c99fbeee47e92d87f73956d42eb031
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2024-05-22 17:03:25 +0200

    Add class for tracking use of function call stack (to check for stack overflows)

---
+++ src/platform/stack_size_tracker.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "debugging.h"
+
+
+// Define StackSizeTracker, recommended to use in global scope
+#define DEFINE_STACK_SIZE_TRACKER(name) static StackSizeTracker name
+
+// Init stack size tracker, use this as in thread entry point function
+#define INIT_STACK_SIZE_TRACKER(name) name.initStackTop(_AddressOfReturnAddress())
+
+// Update tracker, call this in functions called by thread
+#define UPDATE_STACK_SIZE_TRACKER(name) name.update()
+
+
+// Track size of function call stack used (x86 stack grows downwards)
+// This is not byte-accurate, but approximate.
+class StackSizeTracker
+{
+public:
+    static constexpr unsigned long long uninitialized = -1;
+
+    StackSizeTracker()
+    {
+        reset();
+    }
+
+    void reset()
+    {
+        pTop = uninitialized;
+        pBottom = uninitialized;
+    }
+
+    // Call this once passing the top of the stack (first variable at entry point)
+    void initStackTop(void* stackTop)
+    {
+        pTop = (unsigned long long)stackTop;
+    }
+
+    // Update the maximum used stack size. Call this in deep functions
+    void update()
+    {
+        char a;
+        unsigned long long pCurBottom = (unsigned long long) & a;
+        if (pCurBottom < pBottom)
+            pBottom = pCurBottom;
+    }
+
+    // Maximum of stack size used based on calls of update() and initStackTop(),
+    // or unitialized if one of the two functions has not been called.
+    unsigned long long maxStackSize() const
+    {
+        if (pTop == uninitialized || pBottom == uninitialized)
+            return uninitialized;
+        ASSERT(pTop >= pBottom);
+        return pTop - pBottom;
+    }
+
+private:
+    unsigned long long pTop, pBottom;
+};
--- test/platform.cpp
+++ test/platform.cpp
@@ -2,6 +2,7 @@
 
 #include "gtest/gtest.h"
 #include "../src/platform/read_write_lock.h"
+#include "../src/platform/stack_size_tracker.h"
 
 TEST(TestCoreReadWriteLock, SimpleSingleThread)
 {
@@ -35,4 +36,64 @@ TEST(TestCoreReadWriteLock, SimpleSingleThread)
 
     l.acquireWrite();
     l.releaseWrite();
+}
+
+
+StackSizeTracker stackSizeTracker;
+
+template <unsigned int stackVarSize>
+void recursion(int i)
+{
+    int data[stackVarSize];
+    data[0] = 0;
+
+    unsigned long long oldSize = stackSizeTracker.maxStackSize();
+    stackSizeTracker.update();
+    unsigned long long newSize = stackSizeTracker.maxStackSize();
+
+    EXPECT_NE(newSize, StackSizeTracker::uninitialized);
+    EXPECT_GT(newSize, oldSize);
+
+    if (i > 0)
+        recursion<stackVarSize>(i - 1);
+}
+
+template <unsigned int stackVarSize>
+unsigned long long testStackSizeTracker(int recursionLevel)
+{
+    INIT_STACK_SIZE_TRACKER(stackSizeTracker);
+    long long data[10]; data[0] = 0;
+    stackSizeTracker.update();
+    auto size = stackSizeTracker.maxStackSize();
+    {
+        // this does not change size -> shows that storage of all stack variables of function
+        // is reserved, independently of scope
+        long long data2[10]; data2[0] = 0;
+        stackSizeTracker.update();
+        size = stackSizeTracker.maxStackSize();
+    }
+    recursion<stackVarSize>(recursionLevel);
+    size = stackSizeTracker.maxStackSize();
+    stackSizeTracker.reset();
+    return size;
+}
+
+
+TEST(TestCoreStackSizeTracker, SimpleTest)
+{
+    // Return uninit if not not both functions are called
+    DEFINE_STACK_SIZE_TRACKER(s1);
+    EXPECT_EQ(s1.maxStackSize(), StackSizeTracker::uninitialized);
+    INIT_STACK_SIZE_TRACKER(s1);
+    EXPECT_EQ(s1.maxStackSize(), StackSizeTracker::uninitialized);
+    DEFINE_STACK_SIZE_TRACKER(s2);
+    EXPECT_EQ(s2.maxStackSize(), StackSizeTracker::uninitialized);
+    UPDATE_STACK_SIZE_TRACKER(s2);
+    EXPECT_EQ(s2.maxStackSize(), StackSizeTracker::uninitialized);
+
+    // Test that more recursion and more data per function lead to bigger stack size
+    EXPECT_LT(testStackSizeTracker<10>(0), testStackSizeTracker<10>(1));
+    EXPECT_LT(testStackSizeTracker<10>(5), testStackSizeTracker<10>(10));
+    EXPECT_LT(testStackSizeTracker<10>(10), testStackSizeTracker<20>(10));
+    EXPECT_LT(testStackSizeTracker<30>(20), testStackSizeTracker<30>(40));
 }
