7673323024d9a5faf40d45c38ec096f6cc070d06
CFB-QUBIC <cfb@qubic.li>
2024-06-04 11:57:00 +0300

    Added MyLastMatch contract stub.

---
+++ src/contracts/MyLastMatch.h
@@ -0,0 +1,29 @@
+using namespace QPI;
+
+struct MLM2
+{
+};
+
+struct MLM
+{
+	REGISTER_USER_FUNCTIONS_AND_PROCEDURES
+	_
+
+	INITIALIZE
+	_
+
+	BEGIN_EPOCH
+	_
+
+	END_EPOCH
+	_
+
+	BEGIN_TICK
+	_
+
+	END_TICK
+	_
+
+	EXPAND
+	_
+};
