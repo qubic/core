3aaba48d15f4635f782ab0ff4c97cde9fdb359ac
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2024-06-04 13:38:27 +0200

    Reduce duplications of debugging output

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -5410,6 +5410,35 @@ EFI_STATUS efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE* systemTable)
                     }
                     tickerLoopNumerator = 0;
                     tickerLoopDenominator = 0;
+
+                    // output if misalignment happened
+                    if (tickTotalNumberOfComputors - tickNumberOfComputors > QUORUM)
+                    {
+                        if (misalignedState == 0)
+                        {
+                            // also log to debug.log
+                            misalignedState = 1;
+                        }
+                        logToConsole(L"MISALIGNED STATE DETECTED");
+                        if (misalignedState == 1)
+                        {
+                            // print health status and stop repeated logging to debug.log
+                            logHealthStatus();
+                            misalignedState = 2;
+                        }
+                    }
+                    else
+                    {
+                        misalignedState = 0;
+                    }
+
+#if !defined(NDEBUG)
+                    if (system.tick % 1000 == 0)
+                    {
+                        logHealthStatus();
+                    }
+                    printDebugMessages();
+#endif
                 }
                 else
                 {
@@ -5417,32 +5446,7 @@ EFI_STATUS efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE* systemTable)
                     mainLoopDenominator++;
                 }
 
-                // output if misalignment happened
-                if (tickTotalNumberOfComputors - tickNumberOfComputors > QUORUM)
-                {
-                    if (misalignedState == 0)
-                    {
-                        // also log to debug.log
-                        misalignedState = 1;
-                    }
-                    logToConsole(L"MISALIGNED STATE DETECTED");
-                    if (misalignedState == 1)
-                    {
-                        // print health status and stop repeated logging to debug.log
-                        logHealthStatus();
-                        misalignedState = 2;
-                    }
-                }
-                else
-                {
-                    misalignedState = 0;
-                }
-
 #if !defined(NDEBUG)
-                if (system.tick % 1000 == 0)
-                {
-                    logHealthStatus();
-                }
                 printDebugMessages();
 #endif
             }
