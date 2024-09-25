18a4c70b812aa6b3fd803310cd47d479285555c1
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2024-09-25 16:49:44 +0200

    Refactor, preparing to run QX in gtest

--- src/assets/assets.h
+++ src/assets/assets.h
@@ -25,16 +25,14 @@ static char CONTRACT_ASSET_UNIT_OF_MEASUREMENT[7] = { 0, 0, 0, 0, 0, 0, 0 };
 
 static bool initAssets()
 {
-    EFI_STATUS status;
-    if ((status = bs->AllocatePool(EfiRuntimeServicesData, ASSETS_CAPACITY * sizeof(Asset), (void**)&assets))
-        || (status = bs->AllocatePool(EfiRuntimeServicesData, assetDigestsSizeInBytes, (void**)&assetDigests))
-        || (status = bs->AllocatePool(EfiRuntimeServicesData, ASSETS_CAPACITY / 8, (void**)&assetChangeFlags)))
+    if (!allocatePool(ASSETS_CAPACITY * sizeof(Asset), (void**)&assets)
+        || !allocatePool(assetDigestsSizeInBytes, (void**)&assetDigests)
+        || !allocatePool(ASSETS_CAPACITY / 8, (void**)&assetChangeFlags))
     {
-        logStatusToConsole(L"EFI_BOOT_SERVICES.AllocatePool() fails", status, __LINE__);
-
+        logToConsole(L"Failed to allocate asset buffers!");
         return false;
     }
-    bs->SetMem(assetChangeFlags, ASSETS_CAPACITY / 8, 0xFF);
+    setMem(assetChangeFlags, ASSETS_CAPACITY / 8, 0xFF);
     return true;
 }
 
@@ -42,15 +40,18 @@ static void deinitAssets()
 {
     if (assetChangeFlags)
     {
-        bs->FreePool(assetChangeFlags);
+        freePool(assetChangeFlags);
+        assetChangeFlags = nullptr;
     }
     if (assetDigests)
     {
-        bs->FreePool(assetDigests);
+        freePool(assetDigests);
+        assetDigests = nullptr;
     }
     if (assets)
     {
-        bs->FreePool(assets);
+        freePool(assets);
+        assets = nullptr;
     }
 }
 
@@ -66,9 +67,9 @@ iteration:
     {
         assets[*issuanceIndex].varStruct.issuance.publicKey = issuerPublicKey;
         assets[*issuanceIndex].varStruct.issuance.type = ISSUANCE;
-        bs->CopyMem(assets[*issuanceIndex].varStruct.issuance.name, name, sizeof(assets[*issuanceIndex].varStruct.issuance.name));
+        copyMem(assets[*issuanceIndex].varStruct.issuance.name, name, sizeof(assets[*issuanceIndex].varStruct.issuance.name));
         assets[*issuanceIndex].varStruct.issuance.numberOfDecimalPlaces = numberOfDecimalPlaces;
-        bs->CopyMem(assets[*issuanceIndex].varStruct.issuance.unitOfMeasurement, unitOfMeasurement, sizeof(assets[*issuanceIndex].varStruct.issuance.unitOfMeasurement));
+        copyMem(assets[*issuanceIndex].varStruct.issuance.unitOfMeasurement, unitOfMeasurement, sizeof(assets[*issuanceIndex].varStruct.issuance.unitOfMeasurement));
 
         *ownershipIndex = (*issuanceIndex + 1) & (ASSETS_CAPACITY - 1);
     iteration2:
@@ -281,120 +282,6 @@ static void getUniverseDigest(m256i& digest)
 }
 
 
-static void processRequestIssuedAssets(Peer* peer, RequestResponseHeader* header)
-{
-    RespondIssuedAssets response;
-
-    RequestIssuedAssets* request = header->getPayload<RequestIssuedAssets>();
-
-    unsigned int universeIndex = request->publicKey.m256i_u32[0] & (ASSETS_CAPACITY - 1);
-
-    ACQUIRE(universeLock);
-
-iteration:
-    if (universeIndex >= ASSETS_CAPACITY
-        || assets[universeIndex].varStruct.issuance.type == EMPTY)
-    {
-        enqueueResponse(peer, 0, EndResponse::type, header->dejavu(), NULL);
-    }
-    else
-    {
-        if (assets[universeIndex].varStruct.issuance.type == ISSUANCE
-            && assets[universeIndex].varStruct.issuance.publicKey == request->publicKey)
-        {
-            bs->CopyMem(&response.asset, &assets[universeIndex], sizeof(Asset));
-            response.tick = system.tick;
-            response.universeIndex = universeIndex;
-            getSiblings<ASSETS_DEPTH>(response.universeIndex, assetDigests, response.siblings);
-
-            enqueueResponse(peer, sizeof(response), RespondIssuedAssets::type, header->dejavu(), &response);
-        }
-
-        universeIndex = (universeIndex + 1) & (ASSETS_CAPACITY - 1);
-
-        goto iteration;
-    }
-
-    RELEASE(universeLock);
-}
-
-static void processRequestOwnedAssets(Peer* peer, RequestResponseHeader* header)
-{
-    RespondOwnedAssets response;
-
-    RequestOwnedAssets* request = header->getPayload<RequestOwnedAssets>();
-
-    unsigned int universeIndex = request->publicKey.m256i_u32[0] & (ASSETS_CAPACITY - 1);
-
-    ACQUIRE(universeLock);
-
-iteration:
-    if (universeIndex >= ASSETS_CAPACITY
-        || assets[universeIndex].varStruct.issuance.type == EMPTY)
-    {
-        enqueueResponse(peer, 0, EndResponse::type, header->dejavu(), NULL);
-    }
-    else
-    {
-        if (assets[universeIndex].varStruct.issuance.type == OWNERSHIP
-            && assets[universeIndex].varStruct.issuance.publicKey == request->publicKey)
-        {
-            bs->CopyMem(&response.asset, &assets[universeIndex], sizeof(Asset));
-            bs->CopyMem(&response.issuanceAsset, &assets[assets[universeIndex].varStruct.ownership.issuanceIndex], sizeof(Asset));
-            response.tick = system.tick;
-            response.universeIndex = universeIndex;
-            getSiblings<ASSETS_DEPTH>(response.universeIndex, assetDigests, response.siblings);
-
-            enqueueResponse(peer, sizeof(response), RespondOwnedAssets::type, header->dejavu(), &response);
-        }
-
-        universeIndex = (universeIndex + 1) & (ASSETS_CAPACITY - 1);
-
-        goto iteration;
-    }
-
-    RELEASE(universeLock);
-}
-
-static void processRequestPossessedAssets(Peer* peer, RequestResponseHeader* header)
-{
-    RespondPossessedAssets response;
-
-    RequestPossessedAssets* request = header->getPayload<RequestPossessedAssets>();
-
-    unsigned int universeIndex = request->publicKey.m256i_u32[0] & (ASSETS_CAPACITY - 1);
-
-    ACQUIRE(universeLock);
-
-iteration:
-    if (universeIndex >= ASSETS_CAPACITY
-        || assets[universeIndex].varStruct.issuance.type == EMPTY)
-    {
-        enqueueResponse(peer, 0, EndResponse::type, header->dejavu(), NULL);
-    }
-    else
-    {
-        if (assets[universeIndex].varStruct.issuance.type == POSSESSION
-            && assets[universeIndex].varStruct.issuance.publicKey == request->publicKey)
-        {
-            bs->CopyMem(&response.asset, &assets[universeIndex], sizeof(Asset));
-            bs->CopyMem(&response.ownershipAsset, &assets[assets[universeIndex].varStruct.possession.ownershipIndex], sizeof(Asset));
-            bs->CopyMem(&response.issuanceAsset, &assets[assets[assets[universeIndex].varStruct.possession.ownershipIndex].varStruct.ownership.issuanceIndex], sizeof(Asset));
-            response.tick = system.tick;
-            response.universeIndex = universeIndex;
-            getSiblings<ASSETS_DEPTH>(response.universeIndex, assetDigests, response.siblings);
-
-            enqueueResponse(peer, sizeof(response), RespondPossessedAssets::type, header->dejavu(), &response);
-        }
-
-        universeIndex = (universeIndex + 1) & (ASSETS_CAPACITY - 1);
-
-        goto iteration;
-    }
-
-    RELEASE(universeLock);
-}
-
 static bool saveUniverse(CHAR16* directory = NULL)
 {
     logToConsole(L"Saving universe file...");
@@ -429,13 +316,13 @@ static bool loadUniverse(CHAR16* directory = NULL)
     return true;
 }
 
-void assetsEndEpoch()
+static void assetsEndEpoch()
 {
     ACQUIRE(universeLock);
 
     // TODO: comment what is done here
     Asset* reorgAssets = (Asset*)reorgBuffer;
-    bs->SetMem(reorgAssets, ASSETS_CAPACITY * sizeof(Asset), 0);
+    setMem(reorgAssets, ASSETS_CAPACITY * sizeof(Asset), 0);
     for (unsigned int i = 0; i < ASSETS_CAPACITY; i++)
     {
         if (assets[i].varStruct.possession.type == POSSESSION
@@ -454,7 +341,7 @@ void assetsEndEpoch()
             {
                 if (reorgAssets[issuanceIndex].varStruct.issuance.type == EMPTY)
                 {
-                    bs->CopyMem(&reorgAssets[issuanceIndex], &assets[oldIssuanceIndex], sizeof(Asset));
+                    copyMem(&reorgAssets[issuanceIndex], &assets[oldIssuanceIndex], sizeof(Asset));
                 }
 
                 const m256i& ownerPublicKey = assets[oldOwnershipIndex].varStruct.ownership.publicKey;
@@ -514,9 +401,9 @@ void assetsEndEpoch()
             }
         }
     }
-    bs->CopyMem(assets, reorgAssets, ASSETS_CAPACITY * sizeof(Asset));
+    copyMem(assets, reorgAssets, ASSETS_CAPACITY * sizeof(Asset));
 
-    bs->SetMem(assetChangeFlags, ASSETS_CAPACITY / 8, 0xFF);
+    setMem(assetChangeFlags, ASSETS_CAPACITY / 8, 0xFF);
 
     RELEASE(universeLock);
 }
---
+++ src/contract_core/qpi_asset_impl.h
@@ -0,0 +1,248 @@
+#pragma once
+
+#include "contracts/qpi.h"
+
+#include "assets/assets.h"
+
+
+long long QPI::QpiContextProcedureCall::issueAsset(unsigned long long name, const QPI::id& issuer, signed char numberOfDecimalPlaces, long long numberOfShares, unsigned long long unitOfMeasurement) const
+{
+    if (((unsigned char)name) < 'A' || ((unsigned char)name) > 'Z'
+        || name > 0xFFFFFFFFFFFFFF)
+    {
+        return 0;
+    }
+    for (unsigned int i = 1; i < 7; i++)
+    {
+        if (!((unsigned char)(name >> (i * 8))))
+        {
+            while (++i < 7)
+            {
+                if ((unsigned char)(name >> (i * 8)))
+                {
+                    return 0;
+                }
+            }
+
+            break;
+        }
+    }
+    for (unsigned int i = 1; i < 7; i++)
+    {
+        if (!((unsigned char)(name >> (i * 8)))
+            || (((unsigned char)(name >> (i * 8))) >= '0' && ((unsigned char)(name >> (i * 8))) <= '9')
+            || (((unsigned char)(name >> (i * 8))) >= 'A' && ((unsigned char)(name >> (i * 8))) <= 'Z'))
+        {
+            // Do nothing
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    if (issuer != _currentContractId && issuer != _invocator)
+    {
+        return 0;
+    }
+
+    if (numberOfShares <= 0 || numberOfShares > MAX_AMOUNT)
+    {
+        return 0;
+    }
+
+    if (unitOfMeasurement > 0xFFFFFFFFFFFFFF)
+    {
+        return 0;
+    }
+
+    char nameBuffer[7] = { char(name), char(name >> 8), char(name >> 16), char(name >> 24), char(name >> 32), char(name >> 40), char(name >> 48) };
+    char unitOfMeasurementBuffer[7] = { char(unitOfMeasurement), char(unitOfMeasurement >> 8), char(unitOfMeasurement >> 16), char(unitOfMeasurement >> 24), char(unitOfMeasurement >> 32), char(unitOfMeasurement >> 40), char(unitOfMeasurement >> 48) };
+    int issuanceIndex, ownershipIndex, possessionIndex;
+    numberOfShares = ::issueAsset(issuer, nameBuffer, numberOfDecimalPlaces, unitOfMeasurementBuffer, numberOfShares, _currentContractIndex, &issuanceIndex, &ownershipIndex, &possessionIndex);
+
+    return numberOfShares;
+}
+
+
+long long QPI::QpiContextFunctionCall::numberOfPossessedShares(unsigned long long assetName, const m256i& issuer, const m256i& owner, const m256i& possessor, unsigned short ownershipManagingContractIndex, unsigned short possessionManagingContractIndex) const
+{
+    ACQUIRE(universeLock);
+
+    int issuanceIndex = issuer.m256i_u32[0] & (ASSETS_CAPACITY - 1);
+iteration:
+    if (assets[issuanceIndex].varStruct.issuance.type == EMPTY)
+    {
+        RELEASE(universeLock);
+
+        return 0;
+    }
+    else
+    {
+        if (assets[issuanceIndex].varStruct.issuance.type == ISSUANCE
+            && ((*((unsigned long long*)assets[issuanceIndex].varStruct.issuance.name)) & 0xFFFFFFFFFFFFFF) == assetName
+            && assets[issuanceIndex].varStruct.issuance.publicKey == issuer)
+        {
+            int ownershipIndex = owner.m256i_u32[0] & (ASSETS_CAPACITY - 1);
+        iteration2:
+            if (assets[ownershipIndex].varStruct.ownership.type == EMPTY)
+            {
+                RELEASE(universeLock);
+
+                return 0;
+            }
+            else
+            {
+                if (assets[ownershipIndex].varStruct.ownership.type == OWNERSHIP
+                    && assets[ownershipIndex].varStruct.ownership.issuanceIndex == issuanceIndex
+                    && assets[ownershipIndex].varStruct.ownership.publicKey == owner
+                    && assets[ownershipIndex].varStruct.ownership.managingContractIndex == ownershipManagingContractIndex)
+                {
+                    int possessionIndex = possessor.m256i_u32[0] & (ASSETS_CAPACITY - 1);
+                iteration3:
+                    if (assets[possessionIndex].varStruct.possession.type == EMPTY)
+                    {
+                        RELEASE(universeLock);
+
+                        return 0;
+                    }
+                    else
+                    {
+                        if (assets[possessionIndex].varStruct.possession.type == POSSESSION
+                            && assets[possessionIndex].varStruct.possession.ownershipIndex == ownershipIndex
+                            && assets[possessionIndex].varStruct.possession.publicKey == possessor
+                            && assets[possessionIndex].varStruct.possession.managingContractIndex == possessionManagingContractIndex)
+                        {
+                            const long long numberOfPossessedShares = assets[possessionIndex].varStruct.possession.numberOfShares;
+
+                            RELEASE(universeLock);
+
+                            return numberOfPossessedShares;
+                        }
+                        else
+                        {
+                            possessionIndex = (possessionIndex + 1) & (ASSETS_CAPACITY - 1);
+
+                            goto iteration3;
+                        }
+                    }
+                }
+                else
+                {
+                    ownershipIndex = (ownershipIndex + 1) & (ASSETS_CAPACITY - 1);
+
+                    goto iteration2;
+                }
+            }
+        }
+        else
+        {
+            issuanceIndex = (issuanceIndex + 1) & (ASSETS_CAPACITY - 1);
+
+            goto iteration;
+        }
+    }
+}
+
+
+long long QPI::QpiContextProcedureCall::transferShareOwnershipAndPossession(unsigned long long assetName, const m256i& issuer, const m256i& owner, const m256i& possessor, long long numberOfShares, const m256i& newOwnerAndPossessor) const
+{
+    if (numberOfShares <= 0 || numberOfShares > MAX_AMOUNT)
+    {
+        return -((long long)(MAX_AMOUNT + 1));
+    }
+
+    ACQUIRE(universeLock);
+
+    int issuanceIndex = issuer.m256i_u32[0] & (ASSETS_CAPACITY - 1);
+iteration:
+    if (assets[issuanceIndex].varStruct.issuance.type == EMPTY)
+    {
+        RELEASE(universeLock);
+
+        return -numberOfShares;
+    }
+    else
+    {
+        if (assets[issuanceIndex].varStruct.issuance.type == ISSUANCE
+            && ((*((unsigned long long*)assets[issuanceIndex].varStruct.issuance.name)) & 0xFFFFFFFFFFFFFF) == assetName
+            && assets[issuanceIndex].varStruct.issuance.publicKey == issuer)
+        {
+            int ownershipIndex = owner.m256i_u32[0] & (ASSETS_CAPACITY - 1);
+        iteration2:
+            if (assets[ownershipIndex].varStruct.ownership.type == EMPTY)
+            {
+                RELEASE(universeLock);
+
+                return -numberOfShares;
+            }
+            else
+            {
+                if (assets[ownershipIndex].varStruct.ownership.type == OWNERSHIP
+                    && assets[ownershipIndex].varStruct.ownership.issuanceIndex == issuanceIndex
+                    && assets[ownershipIndex].varStruct.ownership.publicKey == owner
+                    && assets[ownershipIndex].varStruct.ownership.managingContractIndex == _currentContractIndex) // TODO: This condition needs extra attention during refactoring!
+                {
+                    int possessionIndex = possessor.m256i_u32[0] & (ASSETS_CAPACITY - 1);
+                iteration3:
+                    if (assets[possessionIndex].varStruct.possession.type == EMPTY)
+                    {
+                        RELEASE(universeLock);
+
+                        return -numberOfShares;
+                    }
+                    else
+                    {
+                        if (assets[possessionIndex].varStruct.possession.type == POSSESSION
+                            && assets[possessionIndex].varStruct.possession.ownershipIndex == ownershipIndex
+                            && assets[possessionIndex].varStruct.possession.publicKey == possessor)
+                        {
+                            if (assets[possessionIndex].varStruct.possession.managingContractIndex == _currentContractIndex) // TODO: This condition needs extra attention during refactoring!
+                            {
+                                if (assets[possessionIndex].varStruct.possession.numberOfShares >= numberOfShares)
+                                {
+                                    int destinationOwnershipIndex, destinationPossessionIndex;
+                                    ::transferShareOwnershipAndPossession(ownershipIndex, possessionIndex, newOwnerAndPossessor, numberOfShares, &destinationOwnershipIndex, &destinationPossessionIndex, false);
+
+                                    RELEASE(universeLock);
+
+                                    return assets[possessionIndex].varStruct.possession.numberOfShares;
+                                }
+                                else
+                                {
+                                    RELEASE(universeLock);
+
+                                    return assets[possessionIndex].varStruct.possession.numberOfShares - numberOfShares;
+                                }
+                            }
+                            else
+                            {
+                                RELEASE(universeLock);
+
+                                return -numberOfShares;
+                            }
+                        }
+                        else
+                        {
+                            possessionIndex = (possessionIndex + 1) & (ASSETS_CAPACITY - 1);
+
+                            goto iteration3;
+                        }
+                    }
+                }
+                else
+                {
+                    ownershipIndex = (ownershipIndex + 1) & (ASSETS_CAPACITY - 1);
+
+                    goto iteration2;
+                }
+            }
+        }
+        else
+        {
+            issuanceIndex = (issuanceIndex + 1) & (ASSETS_CAPACITY - 1);
+
+            goto iteration;
+        }
+    }
+}
