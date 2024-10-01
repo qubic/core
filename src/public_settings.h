01f02c987a1c6bfbe82bb5daef30e5f3bd3459ae
JOETOM <107187448+J0ET0M@users.noreply.github.com>
2024-10-01 22:02:26 +0200

    applied new epoch params

--- src/public_settings.h
+++ src/public_settings.h
@@ -49,12 +49,12 @@
 // Config options that should NOT be changed by operators
 
 #define VERSION_A 1
-#define VERSION_B 220
+#define VERSION_B 221
 #define VERSION_C 0
 
 // Epoch and initial tick for node startup
-#define EPOCH 128
-#define TICK 16100000
+#define EPOCH 129
+#define TICK 16300000
 
 #define ARBITRATOR "AFZPUAIYVPNUYGJRQVLUKOPPVLHAZQTGLYAAUUNBXFTVTAMSBKQBLEIEPCVJ"
 
