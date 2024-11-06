80e9023f6a9a84520e151197e65202185b997079
fnordspace <qubic@fnord.space>
2024-11-06 18:43:15 +0100

    Add function for allocPool with Error msg
    Adds a function for allocating pool memory and automatically logs a
    message if an error occurs.

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -4424,6 +4424,24 @@ static bool saveSystem(CHAR16* directory)
     return false;
 }
 
+
+
+static bool allocPoolWithErrorLog(const EFI_MEMORY_TYPE poolType, const CHAR16* name, const unsigned long long size, void** buffer)
+    {
+    EFI_STATUS status;
+    if (status = bs->AllocatePool(poolType, size, buffer))
+    {
+        setText(message, L"EFI_BOOT_SERVICES.AllocatePool() fails for ");
+        appendText(message, name);
+        appendText(message, L" with size ");
+        appendNumber(message, size, TRUE);
+        logStatusAndMemInfoToConsole(message, status, __LINE__, size);
+        return false;
+    }
+    return true;
+}
+
+
 static bool initialize()
 {
     enableAVX();
