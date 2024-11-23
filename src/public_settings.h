ebd603663954514e347cb2729edbb5f171c1e1af
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2024-11-23 15:49:17 +0100

    Set params for 137 / v1.227.0

--- src/public_settings.h
+++ src/public_settings.h
@@ -49,12 +49,12 @@
 // Config options that should NOT be changed by operators
 
 #define VERSION_A 1
-#define VERSION_B 226
+#define VERSION_B 227
 #define VERSION_C 0
 
 // Epoch and initial tick for node startup
-#define EPOCH 136
-#define TICK 17290000
+#define EPOCH 137
+#define TICK 17420000
 
 #define ARBITRATOR "AFZPUAIYVPNUYGJRQVLUKOPPVLHAZQTGLYAAUUNBXFTVTAMSBKQBLEIEPCVJ"
 
