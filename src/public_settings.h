38ab1c34e5ceb27d89d4ec550c49e797ec817d4d
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2024-12-09 14:23:12 +0100

    update params for epoch 139 / v1.229.0

--- src/public_settings.h
+++ src/public_settings.h
@@ -49,12 +49,12 @@
 // Config options that should NOT be changed by operators
 
 #define VERSION_A 1
-#define VERSION_B 228
+#define VERSION_B 229
 #define VERSION_C 0
 
 // Epoch and initial tick for node startup
-#define EPOCH 138
-#define TICK 17560000
+#define EPOCH 139
+#define TICK 17720000
 
 #define ARBITRATOR "AFZPUAIYVPNUYGJRQVLUKOPPVLHAZQTGLYAAUUNBXFTVTAMSBKQBLEIEPCVJ"
 
