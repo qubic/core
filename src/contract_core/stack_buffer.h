45898e094605e2b05a53865bbdc851411a1c02a1
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2025-02-05 17:52:41 +0100

    Improve StackBuffer
    - improve robustness in case of wrong use and memory corruption
    - add unwind() to support in contract error handling
    - add "special block" flag, to mark blocks that need to be processed
      in error handling

--- src/contract_core/stack_buffer.h
+++ src/contract_core/stack_buffer.h
@@ -4,6 +4,8 @@
 
 // Last-In-First-Out storage for data of different size.
 // Size type used for StackBuffer needs to be unsigned.
+// Supports unwinding for analyzing stack in error handling and tagging blocks as "special" (for example those
+// with infos about locks that need to be released).
 // #define TRACK_MAX_STACK_BUFFER_SIZE to collect info on how much stack is used.
 template <typename StackBufferSizeType, StackBufferSizeType bufferSize>
 struct StackBuffer
@@ -53,7 +55,7 @@ struct StackBuffer
 #endif
 
     // Allocate storage in buffer.
-    char* allocate(SizeType size)
+    char* allocate(SizeType size, bool specialBlock = false)
     {
         ASSERT(_allocatedSize <= bufferSize);
 
@@ -64,7 +66,7 @@ struct StackBuffer
 #ifdef TRACK_MAX_STACK_BUFFER_SIZE
             ++_failedAllocAttempts;
 #endif
-#ifndef NDEBUG
+#if !defined(NDEBUG) && !defined(NO_UEFI)
             CHAR16 dbgMsg[200];
             setText(dbgMsg, L"StackBuffer.allocate() failed! old size ");
             appendNumber(dbgMsg, _allocatedSize, TRUE);
@@ -89,6 +91,8 @@ struct StackBuffer
         // store size from before allocating buffer
         SizeType* sizeBeforeAlloc = reinterpret_cast<SizeType*>(allocatedBuffer + size);
         *sizeBeforeAlloc = _allocatedSize;
+        if (specialBlock)
+            *sizeBeforeAlloc |= specialBlockFlag;
          
         // update size
         _allocatedSize = newSize;
@@ -104,21 +108,27 @@ struct StackBuffer
     // Free storage allocated by last call to allocate().
     bool free()
     {
-        bool okay = (_allocatedSize <= bufferSize) && (_allocatedSize >= sizeof(SizeType));
         // get size before last alloc
+        bool okay = (_allocatedSize <= bufferSize) && (_allocatedSize >= sizeof(SizeType));
+#if !defined(NO_UEFI)
         ASSERT(_allocatedSize <= bufferSize);
         ASSERT(_allocatedSize >= sizeof(SizeType));
-        SizeType sizeBeforeLastAlloc = *reinterpret_cast<SizeType*>(_buffer + _allocatedSize - sizeof(SizeType));
-
-        // reduce current size down to size before last alloc
+#endif
+        SizeType sizeBeforeLastAlloc = *reinterpret_cast<SizeType*>(_buffer + _allocatedSize - sizeof(SizeType)) & sizeMask;
+#if !defined(NO_UEFI)
         ASSERT(sizeBeforeLastAlloc < _allocatedSize);
+#endif
         okay = okay && (sizeBeforeLastAlloc < _allocatedSize);
+
+        // some additional checks and ouput for debugging
 #ifdef TRACK_MAX_STACK_BUFFER_SIZE
+#if !defined(NO_UEFI)
         ASSERT(_maxAllocatedSize <= bufferSize);
         ASSERT(sizeBeforeLastAlloc < _maxAllocatedSize);
+#endif
         okay = okay && (_maxAllocatedSize <= bufferSize) && (sizeBeforeLastAlloc < _maxAllocatedSize);
 #endif
-#ifndef NDEBUG
+#if !defined(NDEBUG) && !defined(NO_UEFI)
         CHAR16 dbgMsg[200];
         if (!okay)
         {
@@ -137,8 +147,19 @@ struct StackBuffer
             addDebugMessage(dbgMsg);
         }
 #endif
-        _allocatedSize = sizeBeforeLastAlloc;
-        return okay;
+        if (okay)
+        {
+            // reduce current size down to size before last alloc
+            // (normal behavior if count of alloc matches count of free and memory is not corrupted)
+            _allocatedSize = sizeBeforeLastAlloc;
+            return true;
+        }
+        else
+        {
+            // problem -> reset to prevent wild undefined behavior in following use
+            _allocatedSize = 0;
+            return false;
+        }
     }
 
     // Free all storage allocated before.
@@ -147,12 +168,35 @@ struct StackBuffer
         _allocatedSize = 0;
     }
 
+    // Free last allocated block and return pointer, size, and whether it is a special block.
+    bool unwind(char*& buffer, SizeType& size, bool& specialBlock)
+    {
+        if (_allocatedSize < sizeof(SizeType))
+            return false;
+        SizeType sizePtrOffset = _allocatedSize - sizeof(SizeType);
+        SizeType prevSizeAndFlag = *reinterpret_cast<SizeType*>(_buffer + sizePtrOffset);
+        size = sizePtrOffset - (prevSizeAndFlag & sizeMask);
+        bool okay = free();
+        specialBlock = (prevSizeAndFlag & specialBlockFlag) != 0;
+        buffer = _buffer + _allocatedSize;
+        return okay;
+    }
+
 protected:
     // structure of buffer content: [ allocated buffer 1 | size before allocating buffer 1 | allocated buffer 2 | size before buffer 2 | ... | alloc. buf. n | size bef. buf. n ]
+    // "size before allocating buffer" may have specialBlockFlag set.
     char _buffer[bufferSize];
 
     // number of bytes used in buffer
     SizeType _allocatedSize;
+
+    // Flag used internally to indicate a special block (bit set in size on _buffer)
+    static constexpr SizeType specialBlockFlag = (1 << (sizeof(StackBufferSizeType) * 8 - 1));
+
+    // Mask to extract size from _buffer
+    static constexpr SizeType sizeMask = ~specialBlockFlag;
+
+    static_assert(bufferSize <= sizeMask, "Buffer size too large for the used size type!");
 
 #ifdef TRACK_MAX_STACK_BUFFER_SIZE
     SizeType _maxAllocatedSize;
