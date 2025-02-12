cb23c455df27b7557640214b0e4b0abf48079cf1
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2025-02-12 11:38:48 +0100

    Fixing previous commit

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -1627,7 +1627,7 @@ static void contractProcessor(void*)
             if (system.epoch == contractDescriptions[executedContractIndex].constructionEpoch
                 && system.epoch < contractDescriptions[executedContractIndex].destructionEpoch)
             {
-                setMem(contractStates[contractIndex], contractDescriptions[contractIndex].stateSize, 0);
+                setMem(contractStates[executedContractIndex], contractDescriptions[executedContractIndex].stateSize, 0);
                 QpiContextSystemProcedureCall qpiContext(executedContractIndex, INITIALIZE);
                 qpiContext.call();
             }
