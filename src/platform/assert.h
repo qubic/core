1f856eb0a34cc61b4801d44cba2a027f79e4df6c
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2025-03-04 11:21:07 +0100

    Simplify finding assert error messages in log

--- src/platform/assert.h
+++ src/platform/assert.h
@@ -15,7 +15,7 @@
 // otherwise, use addDebugMessageAssert() defined in debugging.h
 #define ASSERT(expression) (void)(                                                       \
             (!!(expression)) ||                                                              \
-            (addDebugMessageAssert(_CRT_WIDE(#expression), _CRT_WIDE(__FILE__), (unsigned int)(__LINE__)), 0) \
+            (addDebugMessageAssert(L"ASSERT failed: " _CRT_WIDE(#expression), _CRT_WIDE(__FILE__), (unsigned int)(__LINE__)), 0) \
         )
 
 static void addDebugMessageAssert(const wchar_t* message, const wchar_t* file, const unsigned int lineNumber);
