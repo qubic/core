955ef4d8f0ca39b5e2660ef963255029b38d931d
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-03-10 17:13:34 +0100

    update params for epoch 152 / v1.237.0

--- src/public_settings.h
+++ src/public_settings.h
@@ -49,12 +49,12 @@
 // Config options that should NOT be changed by operators
 
 #define VERSION_A 1
-#define VERSION_B 236
+#define VERSION_B 237
 #define VERSION_C 0
 
 // Epoch and initial tick for node startup
-#define EPOCH 151
-#define TICK 20700000
+#define EPOCH 152
+#define TICK 21180000
 
 #define ARBITRATOR "AFZPUAIYVPNUYGJRQVLUKOPPVLHAZQTGLYAAUUNBXFTVTAMSBKQBLEIEPCVJ"
 #define DISPATCHER "XPXYKFLGSWRHRGAUKWFWVXCDVEYAPCPCNUTMUDWFGDYQCWZNJMWFZEEGCFFO"
