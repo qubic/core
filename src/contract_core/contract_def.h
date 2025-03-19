a94829a6e12660aa100ee1de57f3fcf2ac884473
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-03-19 16:30:21 +0100

    Revert "Add NO_QBAY switch (to be removed later)"
    This reverts commit 3e89b37c65344b951907980a20fcf867f8c95db0.

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -174,7 +174,6 @@ struct __FunctionOrProcedureBeginEndGuard
 #define CONTRACT_STATE2_TYPE MSVAULT2
 #include "contracts/MsVault.h"
 
-#ifndef NO_QBAY
 #undef CONTRACT_INDEX
 #undef CONTRACT_STATE_TYPE
 #undef CONTRACT_STATE2_TYPE
@@ -184,7 +183,6 @@ struct __FunctionOrProcedureBeginEndGuard
 #define CONTRACT_STATE_TYPE QBAY
 #define CONTRACT_STATE2_TYPE QBAY2
 #include "contracts/Qbay.h"
-#endif
 
 // new contracts should be added above this line
 
@@ -261,9 +259,7 @@ constexpr struct ContractDescription
     {"QEARN", 137, 10000, sizeof(QEARN)}, // proposal in epoch 135, IPO in 136, construction in 137 / first donation after END_EPOCH, first round in epoch 138
     {"QVAULT", 138, 10000, sizeof(IPO)}, // proposal in epoch 136, IPO in 137, construction and first use in 138
     {"MSVAULT", 149, 10000, sizeof(MSVAULT)}, // proposal in epoch 147, IPO in 148, construction and first use in 149
-#ifndef NO_QBAY
     {"QBAY", 154, 10000, sizeof(QBAY)}, // proposal in epoch 152, IPO in 153, construction and first use in 154
-#endif
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     {"TESTEXA", 138, 10000, sizeof(IPO)},
@@ -358,9 +354,7 @@ static void initializeContracts()
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QEARN);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QVAULT);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(MSVAULT);
-#ifndef NO_QBAY
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QBAY);
-#endif
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(TESTEXA);
