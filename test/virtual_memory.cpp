9d9d0265c6f20a3bc0ced4b9bda068caf3d681f3
krypdkat <39078779+krypdkat@users.noreply.github.com>
2025-04-01 16:45:10 +0700

    add unit test for special case

--- test/virtual_memory.cpp
+++ test/virtual_memory.cpp
@@ -182,3 +182,58 @@ TEST(TestVirtualMemory, TestVirtualMemory_TickStruct) {
     }
     test_vm.deinit();
 }
+
+TEST(TestVirtualMemory, TestVirtualMemory_SpecialCases) {
+    initFilesystem();
+    registerAsynFileIO(NULL);
+    const unsigned long long name_u64 = 123456789;
+    const unsigned long long pageDir = 0;
+    const unsigned long long pageCap = 2001;
+    VirtualMemory<char, name_u64, pageDir, pageCap, 128> test_vm;
+    test_vm.init();
+    std::vector<char> arr;
+    // fill with random value
+    const int N = 1000000;
+    arr.resize(N);
+    srand(0);
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = int(rand() % 256) - 127;
+    }
+    // append to virtual memory via single append
+    for (int i = 0; i < 113; i++)
+    {
+        test_vm.append(arr[i]);
+    }
+    int pos = 113;
+    int stride = 1337;
+    while (pos < N)
+    {
+        int s = pos;
+        int e = std::min(pos + stride, N);
+        int n_item = e - s;
+        test_vm.appendMany(arr.data() + s, n_item);
+        pos += stride;
+    }
+
+    // case: head start aligns page
+    std::vector<char> fetcher;
+    {
+        int offset = pageCap;
+        int test_len = pageCap * 5 + 1;
+        fetcher.resize(test_len);
+        test_vm.getMany(fetcher.data(), offset, test_len);
+        EXPECT_TRUE(memcmp(fetcher.data(), arr.data() + offset, test_len) == 0);
+    }
+
+    // case: tail end aligns page
+    {
+        int offset = 1337;
+        int test_len = pageCap * 5 + (pageCap - 1337);
+        fetcher.resize(test_len);
+        test_vm.getMany(fetcher.data(), offset, test_len);
+        EXPECT_TRUE(memcmp(fetcher.data(), arr.data() + offset, test_len) == 0);
+    }
+
+    test_vm.deinit();
+}
