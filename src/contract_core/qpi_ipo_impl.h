e55bb4ccbc92822034d6efd661b40551554b3cee
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2025-04-01 12:17:46 +0200

    QPI: add functions to get IPO bids

--- src/contract_core/qpi_ipo_impl.h
+++ src/contract_core/qpi_ipo_impl.h
@@ -23,3 +23,45 @@ bool QPI::QpiContextProcedureCall::bidInIPO(unsigned int IPOContractIndex, long 
 
     return bidInContractIPO(price, quantity, _currentContractId, spectrumIndex, IPOContractIndex, this);
 }
+
+// Returns the ID of the entity who has made this IPO bid or NULL_ID if the ipoContractIndex or ipoBidIndex are invalid.
+QPI::id QPI::QpiContextFunctionCall::ipoBidId(QPI::uint32 ipoContractIndex, QPI::uint32 ipoBidIndex) const
+{
+    if (ipoContractIndex >= contractCount || system.epoch >= contractDescriptions[ipoContractIndex].constructionEpoch || ipoBidIndex >= NUMBER_OF_COMPUTORS)
+    {
+        return NULL_ID;
+    }
+
+    contractStateLock[ipoContractIndex].acquireRead();
+    IPO* ipo = (IPO*)contractStates[ipoContractIndex];
+    QPI::id publicKey = ipo->publicKeys[ipoBidIndex];
+    contractStateLock[ipoContractIndex].releaseRead();
+
+    return publicKey;
+}
+
+// Returns the price of an IPO bid, -1 if contract index is invalid, -2 if contract is not in IPO, -3 if bid index is invalid.
+QPI::sint64 QPI::QpiContextFunctionCall::ipoBidPrice(QPI::uint32 ipoContractIndex, QPI::uint32 ipoBidIndex) const
+{
+    if (ipoContractIndex >= contractCount)
+    {
+        return -1;
+    }
+
+    if (system.epoch >= contractDescriptions[ipoContractIndex].constructionEpoch)
+    {
+        return -2;
+    }
+
+    if (ipoBidIndex >= NUMBER_OF_COMPUTORS)
+    {
+        return -3;
+    }
+
+    contractStateLock[ipoContractIndex].acquireRead();
+    IPO* ipo = (IPO*)contractStates[ipoContractIndex];
+    QPI::sint64 price = ipo->prices[ipoBidIndex];
+    contractStateLock[ipoContractIndex].releaseRead();
+
+    return price;
+}
