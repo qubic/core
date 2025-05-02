8fb2d7dcb8cd684d28ec31b61e3e70416d311975
fnordspace <qubic@fnord.space>
2025-05-02 16:04:03 +0200

    Fix include guard in case clang sets _MSC_VER

--- lib/platform_common/qintrin.h
+++ lib/platform_common/qintrin.h
@@ -2,7 +2,7 @@
 
 // Header file for the inclusion of plartform specific x86/x64 intrinsics header files.
 
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && !defined(__clang__)
 #include <intrin.h>
 #else
 #include <immintrin.h>
