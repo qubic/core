b1830ab6ea48f85474b20e38432c84da1a66ac67
fnordspace <qubic@fnord.space>
2025-05-09 14:30:22 +0200

    Add plotform header file to surpress compiler warnings (#399)
    * Add plotform header file to surpress compiler warnings
    
    Header allows to surpress compiler specific warnings without yielding warnings/errors in other compilers.
    
    Example:
    We need to silent some warning in the clang build. This can be achieved with a `#pragma clang ...`. However this would yield warning when buidling with msvc since this pragma is not known. This header allows to suprress the warnings for clang without the need for guards around every pragma.

---
+++ lib/platform_common/compiler_warnings.h
@@ -0,0 +1,40 @@
+#pragma once
+
+// Macros to suppress specific compiler warnings for specific sections of code.
+// Useful to hide warnings about unknown pragmas while using other compilers.
+
+// Example usage:
+// #include "lib/platform_common/compiler_warnings.h"
+//
+// int foo() {
+//     SUPPRESS_WARNINGS_BEGIN
+//     IGNORE_CAST_ALIGN_WARNING
+//     // Code that generates warnings
+//     SUPPRESS_WARNINGS_END
+//    return 0;
+// }
+
+
+#if defined(__clang__)
+    #define PRAGMA(x) _Pragma(#x)
+    #define WARNING_PUSH PRAGMA(clang diagnostic push)
+    #define WARNING_POP PRAGMA(clang diagnostic pop)
+    #define WARNING_IGNORE_CAST_ALIGN PRAGMA(clang diagnostic ignored "-Wcast-align")
+    #define WARNING_IGNORE_UNUSED PRAGMA(clang diagnostic ignored "-Wunused-parameter")
+    #define WARNING_IGNORE_SELFASSIGNMENT PRAGMA(clang diagnostic ignored "-Wself-assign-overloaded")
+
+#else
+    #define WARNING_PUSH
+    #define WARNING_POP
+    #define WARNING_IGNORE_CAST_ALIGN
+    #define WARNING_IGNORE_UNUSED
+    #define WARNING_IGNORE_SELFASSIGNMENT
+#endif
+
+// Shortcuts
+#define SUPPRESS_WARNINGS_BEGIN WARNING_PUSH
+#define SUPPRESS_WARNINGS_END WARNING_POP
+#define IGNORE_CAST_ALIGN_WARNING WARNING_IGNORE_CAST_ALIGN 
+#define IGNORE_UNUSED_WARNING WARNING_IGNORE_UNUSED
+#define IGNORE_SELFASSIGNMENT_WARNING WARNING_IGNORE_SELFASSIGNMENT
+
