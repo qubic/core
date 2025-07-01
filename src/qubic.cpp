31197f618822f69abe881d189b2bfc1ca04911a2
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2025-07-01 16:10:57 +0200

    Fix error check + output of loading contract states

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -5239,7 +5239,6 @@ static void contractProcessorShutdownCallback(EFI_EVENT Event, void* Context)
 static bool loadComputer(CHAR16* directory, bool forceLoadFromFile)
 {
     logToConsole(L"Loading contract files ...");
-    setText(message, L"Loaded SC: ");
     for (unsigned int contractIndex = 0; contractIndex < contractCount; contractIndex++)
     {
         if (contractDescriptions[contractIndex].constructionEpoch == system.epoch && !forceLoadFromFile)
@@ -5253,29 +5252,26 @@ static bool loadComputer(CHAR16* directory, bool forceLoadFromFile)
             CONTRACT_FILE_NAME[sizeof(CONTRACT_FILE_NAME) / sizeof(CONTRACT_FILE_NAME[0]) - 7] = (contractIndex % 100) / 10 + L'0';
             CONTRACT_FILE_NAME[sizeof(CONTRACT_FILE_NAME) / sizeof(CONTRACT_FILE_NAME[0]) - 6] = contractIndex % 10 + L'0';
             long long loadedSize = load(CONTRACT_FILE_NAME, contractDescriptions[contractIndex].stateSize, contractStates[contractIndex], directory);
+            setText(message, L" -> ");
+            appendText(message, CONTRACT_FILE_NAME);
             if (loadedSize != contractDescriptions[contractIndex].stateSize)
             {
                 if (system.epoch < contractDescriptions[contractIndex].constructionEpoch && contractDescriptions[contractIndex].stateSize >= sizeof(IPO))
                 {
                     setMem(contractStates[contractIndex], contractDescriptions[contractIndex].stateSize, 0);
-                    appendText(message, L"(");
-                    appendText(message, CONTRACT_FILE_NAME);
-                    appendText(message, L" not loaded but initialized with zeros for IPO) ");
+                    appendText(message, L" not loaded but initialized with zeros for IPO");
                 }
                 else
                 {
+                    appendText(message, L" cannot be read successfully");
+                    logToConsole(message);
                     logStatusToConsole(L"EFI_FILE_PROTOCOL.Read() reads invalid number of bytes", loadedSize, __LINE__);
                     return false;
                 }
             }
-            else
-            {
-                appendText(message, CONTRACT_FILE_NAME);
-                appendText(message, L" ");
-            }
+            logToConsole(message);
         }
     }
-    logToConsole(message);
     return true;
 }
 
@@ -5556,7 +5552,8 @@ static bool initialize()
                 appendText(message, L".");
                 logToConsole(message);
             }
-            loadComputer();
+            if (!loadComputer())
+                return false;
             m256i computerDigest;
             {
                 setText(message, L"Computer digest = ");
