ab225d483e8ddcf94fffd44ec08ca0439e2d1615
small-debug <mathkindom@gmail.com>
2025-07-12 03:54:25 +0900

    setting the initial epoch

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -289,7 +289,7 @@ constexpr struct ContractDescription
     {"QVAULT", 138, 10000, sizeof(IPO)}, // proposal in epoch 136, IPO in 137, construction and first use in 138
     {"MSVAULT", 149, 10000, sizeof(MSVAULT)}, // proposal in epoch 147, IPO in 148, construction and first use in 149
     {"QBAY", 154, 10000, sizeof(QBAY)}, // proposal in epoch 152, IPO in 153, construction and first use in 154
-    {"NOST", 168, 10000, sizeof(NOST)}, // proposal in epoch 166, IPO in 167, construction and first use in 168
+    {"NOST", 171, 10000, sizeof(NOST)}, // proposal in epoch 169, IPO in 170, construction and first use in 171
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     {"TESTEXA", 138, 10000, sizeof(IPO)},
