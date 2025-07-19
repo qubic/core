3f38f6844b07f6d37baed4e9cd6fceca254c8e23
wm <162594684+small-debug@users.noreply.github.com>
2025-07-19 16:20:22 +0900

    NOSTROMO LAUNCHPAD SMART CONTRACT (#437)
    * NOSTROMO smart contract
    
    * Google Test For Nostromo
    
    * setting the testnet
    
    * increase max number of token
    
    * increase the max number of token
    
    * setting initial epoch
    
    * resolve compile error
    
    * implement HashMap for Users
    
    * Using get() instead of contains()
    
    * Revert "Merge branch 'NOST-NEW-TESTNET' into main"
    
    This reverts commit 010585bde6e1a4b18463e1b422b998a8caf25182, reversing
    changes made to d539a6b090381a7d40fe37094af8c6d9777f196b.
    
    * add team address to transfer 10% revenue
    
    * Add EndEpochSucceedFundaraising Checker
    
    * Add EndEpochFailedFundaraising Checker
    
    * Final test version
    
    * setting the initial epoch
    
    * change getCurrentDate() to QUOTTERY::packQuotteryDate()
    
    * remove getCurrentDate()
    
    * update the schedule
    
    * improving code style
    
    * changed QX fee

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -194,6 +194,16 @@ struct __FunctionOrProcedureBeginEndGuard
 #define CONTRACT_STATE2_TYPE QSWAP2
 #include "contracts/Qswap.h"
 
+#undef CONTRACT_INDEX
+#undef CONTRACT_STATE_TYPE
+#undef CONTRACT_STATE2_TYPE
+
+#define NOST_CONTRACT_INDEX 14
+#define CONTRACT_INDEX NOST_CONTRACT_INDEX
+#define CONTRACT_STATE_TYPE NOST
+#define CONTRACT_STATE2_TYPE NOST2
+#include "contracts/Nostromo.h"
+
 // new contracts should be added above this line
 
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
@@ -290,6 +300,7 @@ constexpr struct ContractDescription
     {"MSVAULT", 149, 10000, sizeof(MSVAULT)}, // proposal in epoch 147, IPO in 148, construction and first use in 149
     {"QBAY", 154, 10000, sizeof(QBAY)}, // proposal in epoch 152, IPO in 153, construction and first use in 154
     {"QSWAP", 171, 10000, sizeof(QSWAP)}, // proposal in epoch 169, IPO in 170, construction and first use in 171
+    {"NOST", 172, 10000, sizeof(NOST)}, // proposal in epoch 170, IPO in 171, construction and first use in 172
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     {"TESTEXA", 138, 10000, sizeof(IPO)},
@@ -392,6 +403,7 @@ static void initializeContracts()
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(MSVAULT);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QBAY);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QSWAP);
+    REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(NOST);
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(TESTEXA);
