6fcdd63014b353f646a880f3988264d62c4a25ce
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-07-29 10:17:41 +0200

    Revert "add NO_NOST toggle"
    This reverts commit fc473bd121f05f64095b7a9fdebebd6f56c4ef84.

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -1,7 +1,5 @@
 #define SINGLE_COMPILE_UNIT
 
-// #define NO_NOST
-
 // contract_def.h needs to be included first to make sure that contracts have minimal access
 #include "contract_core/contract_def.h"
 #include "contract_core/contract_exec.h"
