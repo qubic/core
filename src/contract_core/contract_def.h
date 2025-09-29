f3cd543dd3dbee4b6893ae1f47c1346d41244097
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-09-29 14:03:24 +0200

    add NO_RANDOM_LOTTERY toggle

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -218,15 +218,19 @@ struct __FunctionOrProcedureBeginEndGuard
 #define CONTRACT_STATE2_TYPE QDRAW2
 #include "contracts/Qdraw.h"
 
+#ifndef NO_RANDOM_LOTTERY
+
+constexpr unsigned short RL_CONTRACT_INDEX = (CONTRACT_INDEX + 1);
 #undef CONTRACT_INDEX
 #undef CONTRACT_STATE_TYPE
 #undef CONTRACT_STATE2_TYPE
 
-#define RL_CONTRACT_INDEX 16
 #define CONTRACT_INDEX RL_CONTRACT_INDEX
 #define CONTRACT_STATE_TYPE RL
 #define CONTRACT_STATE2_TYPE RL2
 #include "contracts/RandomLottery.h"
+
+#endif
 
 // new contracts should be added above this line
 
@@ -326,7 +330,9 @@ constexpr struct ContractDescription
     {"QSWAP", 171, 10000, sizeof(QSWAP)}, // proposal in epoch 169, IPO in 170, construction and first use in 171
     {"NOST", 172, 10000, sizeof(NOST)}, // proposal in epoch 170, IPO in 171, construction and first use in 172
     {"QDRAW", 179, 10000, sizeof(QDRAW)}, // proposal in epoch 177, IPO in 178, construction and first use in 179
+#ifndef NO_RANDOM_LOTTERY
     {"RL", 182, 10000, sizeof(RL)}, // proposal in epoch 180, IPO in 181, construction and first use in 182
+#endif
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     {"TESTEXA", 138, 10000, sizeof(IPO)},
@@ -431,7 +437,9 @@ static void initializeContracts()
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QSWAP);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(NOST);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QDRAW);
+#ifndef NO_RANDOM_LOTTERY
 	REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(RL);
+#endif
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(TESTEXA);
