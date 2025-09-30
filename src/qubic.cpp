687d488f0fb4ed05879debad7bf7d09252b574f8
feiyu <77367770+hackerby888@users.noreply.github.com>
2025-09-30 04:10:40 -0400

    Update qubic.cpp

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -21,7 +21,7 @@
 // this option enables using disk as RAM to reduce hardware requirement for qubic core node
 // it is highly recommended to enable this option if you want to run a full mainnet node on SSD
 // UNCOMMENT this line to enable it
-#define USE_SWAP
+// #define USE_SWAP
 
 //////////////////////////////////////////////////////////////
 
@@ -8016,4 +8016,5 @@ int main(int argc, const char* argv[]) {
     Overload::initializeUefi();
     return (int)efi_main(ih, st);
 }
+
 
