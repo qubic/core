1ca0fa899af0000c7b3a9e1a9667181ad78c08c0
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-10-27 15:29:44 +0100

    update params for epoch 185 / v1.266.0

--- src/public_settings.h
+++ src/public_settings.h
@@ -64,12 +64,12 @@ static_assert(AUTO_FORCE_NEXT_TICK_THRESHOLD* TARGET_TICK_DURATION >= PEER_REFRE
 // Config options that should NOT be changed by operators
 
 #define VERSION_A 1
-#define VERSION_B 265
+#define VERSION_B 266
 #define VERSION_C 0
 
 // Epoch and initial tick for node startup
-#define EPOCH 184
-#define TICK 35340000
+#define EPOCH 185
+#define TICK 35891000
 #define TICK_IS_FIRST_TICK_OF_EPOCH 1 // Set to 0 if the network is restarted during the EPOCH with a new initial TICK
 
 #define ARBITRATOR "AFZPUAIYVPNUYGJRQVLUKOPPVLHAZQTGLYAAUUNBXFTVTAMSBKQBLEIEPCVJ"
