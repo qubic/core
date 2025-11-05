04094f88ef638062dbfa62bc3d23e6a4244bbd23
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-11-05 14:01:06 +0100

    Revert "add toggle RL_V1"
    This reverts commit f8a45b8eea7f4746055f81caded1cddd83a9bc34.

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -1,7 +1,6 @@
 #define SINGLE_COMPILE_UNIT
 
 // #define CCF_BY_ANYONE
-// #define RL_V1
 
 // contract_def.h needs to be included first to make sure that contracts have minimal access
 #include "contract_core/contract_def.h"
