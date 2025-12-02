0acf4467f48ae692b1b91bbb7345b29bf33f50fc
.IV <77367770+hackerby888@users.noreply.github.com>
2025-12-02 12:55:53 +0700

    Update public_settings.h

--- src/public_settings.h
+++ src/public_settings.h
@@ -73,7 +73,7 @@ static_assert(AUTO_FORCE_NEXT_TICK_THRESHOLD* TARGET_TICK_DURATION >= PEER_REFRE
 // If this flag is 1, it indicates that the whole network (all 676 IDs) will start from scratch and agree that the very first tick time will be set at (2022-04-13 Wed 12:00:00.000UTC).
 // If this flag is 0, the node will try to fetch data of the initial tick of the epoch from other nodes, because the tick's timestamp may differ from (2022-04-13 Wed 12:00:00.000UTC).
 // If you restart your node after seamless epoch transition, make sure EPOCH and TICK are set correctly for the currently running epoch.
-#define START_NETWORK_FROM_SCRATCH 0
+#define START_NETWORK_FROM_SCRATCH 1
 #ifdef TESTNET
 #undef START_NETWORK_FROM_SCRATCH
 #define START_NETWORK_FROM_SCRATCH 1
@@ -151,3 +151,4 @@ static unsigned int gFullExternalComputationTimes[][2] =
 
 #define STACK_SIZE 4194304
 #define TRACK_MAX_STACK_BUFFER_SIZE
+
