ef41b57eec680d43dbe3519c7652de0373bc6351
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2025-12-16 10:20:31 +0100

    virtual memory test: add deinit filesystem

--- test/virtual_memory.cpp
+++ test/virtual_memory.cpp
@@ -55,6 +55,8 @@ TEST(TestVirtualMemory, TestVirtualMemory_NativeChar) {
         EXPECT_TRUE(test_vm[index] == arr[index]);
     }
     test_vm.deinit();
+
+    deInitFileSystem();
 }
 
 #define IMAX_BITS(m) ((m)/((m)%255+1) / 255%255*8 + 7-86/((m)%255+12))
@@ -116,6 +118,8 @@ TEST(TestVirtualMemory, TestVirtualMemory_NativeU64) {
         EXPECT_TRUE(test_vm[index] == arr[index]);
     }
     test_vm.deinit();
+
+    deInitFileSystem();
 }
 
 TickData randTick()
@@ -181,6 +185,8 @@ TEST(TestVirtualMemory, TestVirtualMemory_TickStruct) {
         EXPECT_TRUE(tickEqual(test_vm[index], arr[index]));
     }
     test_vm.deinit();
+
+    deInitFileSystem();
 }
 
 TEST(TestVirtualMemory, TestVirtualMemory_SpecialCases) {
@@ -236,4 +242,6 @@ TEST(TestVirtualMemory, TestVirtualMemory_SpecialCases) {
     }
 
     test_vm.deinit();
+
+    deInitFileSystem();
 }
