9ae79bac0426320d25e90f3f15c5b814ba5d2d0e
cyber-pc <cyber-pc@blue-pc.localhost>
2025-12-31 19:54:48 +0700

    update params for epoch 194 / v1.272.2

--- src/public_settings.h
+++ src/public_settings.h
@@ -67,11 +67,11 @@ static_assert(AUTO_FORCE_NEXT_TICK_THRESHOLD* TARGET_TICK_DURATION >= PEER_REFRE
 
 #define VERSION_A 1
 #define VERSION_B 272
-#define VERSION_C 1
+#define VERSION_C 2
 
 // Epoch and initial tick for node startup
-#define EPOCH 193
-#define TICK 40438468
+#define EPOCH 194
+#define TICK 41033983
 #define TICK_IS_FIRST_TICK_OF_EPOCH 1 // Set to 0 if the network is restarted during the EPOCH with a new initial TICK
 
 #define ARBITRATOR "AFZPUAIYVPNUYGJRQVLUKOPPVLHAZQTGLYAAUUNBXFTVTAMSBKQBLEIEPCVJ"
