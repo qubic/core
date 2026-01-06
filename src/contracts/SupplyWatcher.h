9bbb0d33f1da2f9778e52a1c8f633f4f203f882e
fnordspace <qubic@fnord.space>
2026-01-06 17:25:17 +0100

    Adjust SWATCH to burn for all infrastructure contracts

--- src/contracts/SupplyWatcher.h
+++ src/contracts/SupplyWatcher.h
@@ -14,17 +14,24 @@ struct SWATCH : public ContractBase
     {
         Entity ownEntity;
         sint64 ownBalance;
+        sint64 burnAmountPerContract;
     };
 
     BEGIN_EPOCH_WITH_LOCALS()
     {
         // Burn all coins of this contract. According to agreement of the quorum, a part of the
         // computor revenue is donated to this contract for burning.
+        // Burns are split equally between GQMPROP (6), SWATCH (7), and CCF (8).
         if (qpi.getEntity(SELF, locals.ownEntity))
         {
             locals.ownBalance = locals.ownEntity.incomingAmount - locals.ownEntity.outgoingAmount;
             if (locals.ownBalance > 0)
-                qpi.burn(locals.ownBalance);
+            {
+                locals.burnAmountPerContract = locals.ownBalance / 3;
+                qpi.burn(locals.burnAmountPerContract, 6);  // GQMPROP
+                qpi.burn(locals.burnAmountPerContract, 7);  // SWATCH
+                qpi.burn(locals.burnAmountPerContract, 8);  // CCF
+            }
         }
     }
 };
