129010719a9395db02f2700b71c6a7965787caeb
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2026-01-12 14:49:06 +0100

    qRWA: change construction epoch from 198 to 197 (as confirmed with Eko)

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -318,7 +318,7 @@ constexpr struct ContractDescription
     {"QBOND", 182, 10000, sizeof(QBOND)}, // proposal in epoch 180, IPO in 181, construction and first use in 182
     {"QIP", 189, 10000, sizeof(QIP)}, // proposal in epoch 187, IPO in 188, construction and first use in 189
     {"QRAFFLE", 192, 10000, sizeof(QRAFFLE)}, // proposal in epoch 190, IPO in 191, construction and first use in 192
-    {"QRWA", 198, 10000, sizeof(QRWA)}, // proposal in epoch 196, IPO in 197, construction and first use in 198
+    {"QRWA", 197, 10000, sizeof(QRWA)}, // proposal in epoch 195, IPO in 196, construction and first use in 197
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     {"TESTEXA", 138, 10000, sizeof(TESTEXA)},
