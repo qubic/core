87d2cdb6dcc2cffba8fe5eb866d79d4e8a4bbf24
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2026-01-14 15:52:32 +0100

    Revert "add NO_QRWA toggle"
    This reverts commit e9293e7cce0d7f18e7db87aa11d762260deb72e7.

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -201,8 +201,6 @@
 #define CONTRACT_STATE2_TYPE QRAFFLE2
 #include "contracts/QRaffle.h"
 
-#ifndef NO_QRWA
-
 #undef CONTRACT_INDEX
 #undef CONTRACT_STATE_TYPE
 #undef CONTRACT_STATE2_TYPE
@@ -212,8 +210,6 @@
 #define CONTRACT_STATE_TYPE QRWA
 #define CONTRACT_STATE2_TYPE QRWA2
 #include "contracts/qRWA.h"
-
-#endif
 
 // new contracts should be added above this line
 
@@ -322,9 +318,7 @@ constexpr struct ContractDescription
     {"QBOND", 182, 10000, sizeof(QBOND)}, // proposal in epoch 180, IPO in 181, construction and first use in 182
     {"QIP", 189, 10000, sizeof(QIP)}, // proposal in epoch 187, IPO in 188, construction and first use in 189
     {"QRAFFLE", 192, 10000, sizeof(QRAFFLE)}, // proposal in epoch 190, IPO in 191, construction and first use in 192
-#ifndef NO_QRWA
     {"QRWA", 197, 10000, sizeof(QRWA)}, // proposal in epoch 195, IPO in 196, construction and first use in 197
-#endif
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     {"TESTEXA", 138, 10000, sizeof(TESTEXA)},
@@ -440,9 +434,7 @@ static void initializeContracts()
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QBOND);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QIP);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QRAFFLE);
-#ifndef NO_QRWA
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QRWA);
-#endif
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(TESTEXA);
