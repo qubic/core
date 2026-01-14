577ede880e5d8d54d65110989bebb51712a87dbb
cyber-pc <165458555+cyber-pc@users.noreply.github.com>
2026-01-14 10:12:15 +0700

    Fix missing dst in text_output.

--- src/text_output.h
+++ src/text_output.h
@@ -19,38 +19,38 @@ static void appendQubicVersion(CHAR16* dst)
 
 static void appendIPv4Address(CHAR16* dst, const IPv4Address& address)
 {
-    appendNumber(message, address.u8[0], FALSE);
-    appendText(message, L".");
-    appendNumber(message, address.u8[1], FALSE);
-    appendText(message, L".");
-    appendNumber(message, address.u8[2], FALSE);
-    appendText(message, L".");
-    appendNumber(message, address.u8[3], FALSE);
+    appendNumber(dst, address.u8[0], FALSE);
+    appendText(dst, L".");
+    appendNumber(dst, address.u8[1], FALSE);
+    appendText(dst, L".");
+    appendNumber(dst, address.u8[2], FALSE);
+    appendText(dst, L".");
+    appendNumber(dst, address.u8[3], FALSE);
 }
 
 static void appendDateAndTime(CHAR16* dst, const QPI::DateAndTime& dt, bool microsec = false)
 {
-    appendNumber(message, dt.getYear(), FALSE);
-    appendText(message, L"-");
-    appendNumber(message, dt.getMonth(), FALSE);
-    appendText(message, L"-");
-    appendNumber(message, dt.getDay(), FALSE);
-    appendText(message, L" ");
-    appendNumber(message, dt.getHour(), FALSE);
-    appendText(message, L":");
-    appendNumber(message, dt.getMinute(), FALSE);
-    appendText(message, L":");
-    appendNumber(message, dt.getSecond(), FALSE);
-    appendText(message, L".");
+    appendNumber(dst, dt.getYear(), FALSE);
+    appendText(dst, L"-");
+    appendNumber(dst, dt.getMonth(), FALSE);
+    appendText(dst, L"-");
+    appendNumber(dst, dt.getDay(), FALSE);
+    appendText(dst, L" ");
+    appendNumber(dst, dt.getHour(), FALSE);
+    appendText(dst, L":");
+    appendNumber(dst, dt.getMinute(), FALSE);
+    appendText(dst, L":");
+    appendNumber(dst, dt.getSecond(), FALSE);
+    appendText(dst, L".");
     const uint16 millisec = dt.getMillisec();
-    appendNumber(message, millisec / 100, FALSE);
-    appendNumber(message, (millisec % 100) / 10, FALSE);
-    appendNumber(message, millisec % 10, FALSE);
+    appendNumber(dst, millisec / 100, FALSE);
+    appendNumber(dst, (millisec % 100) / 10, FALSE);
+    appendNumber(dst, millisec % 10, FALSE);
     if (microsec)
     {
         const uint16 microsec = dt.getMicrosecDuringMillisec();
-        appendNumber(message, microsec / 100, FALSE);
-        appendNumber(message, (microsec % 100) / 10, FALSE);
-        appendNumber(message, microsec % 10, FALSE);
+        appendNumber(dst, microsec / 100, FALSE);
+        appendNumber(dst, (microsec % 100) / 10, FALSE);
+        appendNumber(dst, microsec % 10, FALSE);
     }
 }
