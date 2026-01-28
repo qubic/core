bf033093612befd24855718e791f8aafe8b9fcd4
cyber-pc <165458555+cyber-pc@users.noreply.github.com>
2026-01-28 21:34:01 +0700

    Add ability to adjust multipler of each mining algo's solution. (#721)
    * Add ability to adjust multipler of each mining algo's solution.
    
    * Set ADDITION multiplier equal to HYPERIDENTITY.

--- src/public_settings.h
+++ src/public_settings.h
@@ -104,6 +104,10 @@ static constexpr unsigned long long ADDITION_NUMBER_OF_MUTATIONS = 150;
 static constexpr unsigned long long ADDITION_POPULATION_THRESHOLD = ADDITION_NUMBER_OF_INPUT_NEURONS + ADDITION_NUMBER_OF_OUTPUT_NEURONS + ADDITION_NUMBER_OF_MUTATIONS; // P
 static constexpr unsigned int ADDITION_SOLUTION_THRESHOLD_DEFAULT = 74200;
 
+// Multipler of score
+static constexpr unsigned int HYPERIDENTITY_SOLUTION_MULTIPLER = 1;
+static constexpr unsigned int ADDITION_SOLUTION_MULTIPLER = 1;
+
 static constexpr long long NEURON_VALUE_LIMIT = 1LL;
 
 
