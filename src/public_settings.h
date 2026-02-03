07e919dd6229fadb61a17bfbe199bb1b9026ec76
cyber-pc <cyber-pc@blue-pc.localhost>
2026-02-03 08:44:04 +0700

    Reduce ADDITION_SOLUTION_THRESHOLD_DEFAULT to 74194.

--- src/public_settings.h
+++ src/public_settings.h
@@ -102,7 +102,7 @@ static constexpr unsigned long long ADDITION_NUMBER_OF_TICKS = 1000;            
 static constexpr unsigned long long ADDITION_NUMBER_OF_NEIGHBORS = 728;    // 2M. Must be divided by 2
 static constexpr unsigned long long ADDITION_NUMBER_OF_MUTATIONS = 150;
 static constexpr unsigned long long ADDITION_POPULATION_THRESHOLD = ADDITION_NUMBER_OF_INPUT_NEURONS + ADDITION_NUMBER_OF_OUTPUT_NEURONS + ADDITION_NUMBER_OF_MUTATIONS; // P
-static constexpr unsigned int ADDITION_SOLUTION_THRESHOLD_DEFAULT = 74200;
+static constexpr unsigned int ADDITION_SOLUTION_THRESHOLD_DEFAULT = 74194;
 
 // Multipler of score
 static constexpr unsigned int HYPERIDENTITY_SOLUTION_MULTIPLER = 1;
