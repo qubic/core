7b14960632804802ed5e64263b9801f45d38e42e
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2026-02-06 15:08:01 +0100

    Disable test contract / contract queries

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -1,6 +1,6 @@
 #define SINGLE_COMPILE_UNIT
 
-#define INCLUDE_CONTRACT_TEST_EXAMPLES
+//#define INCLUDE_CONTRACT_TEST_EXAMPLES
 
 // contract_def.h needs to be included first to make sure that contracts have minimal access
 #include "contract_core/contract_def.h"
