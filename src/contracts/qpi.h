e9fe10dda58622c64824eb85f3fe3bcc9a9f4992
Philipp Werner <22914157+philippwerner@users.noreply.github.com>
2026-02-06 09:36:36 +0100

    Add new price oracles + change TESTEXC queries

--- src/contracts/qpi.h
+++ src/contracts/qpi.h
@@ -77,6 +77,7 @@ namespace QPI
 		{
 			null = 0,
 			space = ' ', slash = '/', backslash = '\\', dot = '.', comma = ',', colon = ':', semicolon = ';',
+			underscore = '_', minus = '-', plus = '+', star = '*', dollar = '$', question_mark = '?', exclamation_mark = '!',
 			a = 'a', b = 'b', c = 'c', d = 'd', e = 'e', f = 'f', g = 'g', h = 'h', i = 'i', j = 'j', k = 'k', l = 'l', m = 'm',
 			n = 'n', o = 'o', p = 'p', q = 'q', r = 'r', s = 's', t = 't', u = 'u', v = 'v', w = 'w', x = 'x', y = 'y', z = 'z',
 			A = 'A', B = 'B', C = 'C', D = 'D', E = 'E', F = 'F', G = 'G', H = 'H', I = 'I', J = 'J', K = 'K', L = 'L', M = 'M',
