498d4fd515f8b77dd1dafbbc31d11a8032371664
TakaYuPP <yutotaka24@gmail.com>
2026-03-10 00:44:07 -0700

    updated Qvault SC (#365)
    * fix: updated QVAULT SC
    
    * feat: added getData functions
    
    * fix: fixed some bugs in submitProposal procedures
    
    * fix: voteInProposal
    
    * fix: fixed type of index in for loop
    
    * fix: fixed revenue distribution
    
    * fix: fixed type of variable in for loop
    
    * fix: fixed contract_qvault.cpp
    
    * Add burning of asset shares
    
    Extend transferShareOwnershipAndPossession() to support burning of asset
    shares by transferring to NULL_ID. Burning contract shares is forbidden.
    
    * final update for burning the Qcap
    
    * feat: added new functions for fetching the data
    
    * fix: fixed type convert errors
    
    * feat: added test functions
    
    * fix: fixed some errors according to gtest
    
    * Always reserve contract stack for system procedures
    
    Even if the contract stack is not needed for storing locals, inputs, or
    outputs directly when starting a system procedure, it may be needed by
    nested calls of QPI / procedures / functions. So we always need to
    reserve a contract stack.
    
    * updated the SC finally
    
    * fix: removed unnecessary state
    
    * fix: fixed some data type
    
    * feat: added cancelMuslimId procedure
    
    * fix: fixed for Phil's review
    
    * fix: some ambiguous
    
    * fix: cut duplicated votes
    
    * fix: updated the QCP & AlloP & MKTP
    
    * added the url for proposal and changed team fee end date
    
    * fix: added cleanupIfNeeded() function for HashMap
    
    * fix: removed Muslim features
    
    * fix: removed a variable for muslim feature
    
    * fix: removed a struct for muslim feature
    
    * fix: fixed payment structure
    
    * fix: fixed some order for initialization
    
    * fix: removed reinvestingAddress
    
    * fix: excepted SELF address in calc the number of qcap holders
    
    * fix: fixed some distribution issues
    
    * fix: fixed the circulating supply
    
    * feat: added the revenue to send manually
    
    * Revert "fix: added cleanupIfNeeded() function for HashMap"
    
    This reverts commit 579f8beb6d2018274410f7f5a9d6235f4366e963.
    
    * Reapply "fix: added cleanupIfNeeded() function for HashMap"
    
    This reverts commit ed47ca15442a5411d4d78cd043d18bab14e05998.
    
    * Update init of QX fees to fix gtest
    
    * fix: fixed node breaking & getting the revenue from standard tx & output issue
    
    * fix: counts staked amount in getNumberOfHolderAndAvgAm
    
    * fix: fixed the initial result status of proposals
    
    * fix: change the decision in vote
    
    * fix: 1 Qcap buy issue
    
    * fix: resolved change vote issue
    
    * fix: fixed the getAmountForQearnInUpcomingEpoch
    
    * added comments for all the functions and profedures
    
    * fix: removed :: which it is restricted in development of qubic smart contract
    
    * fix: removed QPI::QpiContext
    
    * fix: fixed some parts by audit
    
    * fix: second commit for audit
    
    * fix: update for the rev2 audit
    
    * fix: Qearn revenue issue
    
    * fix: fixed the values for the initialize variables
    
    * fix: fixed the bug in the voteInProposal precedure
    
    * fix: fixed the bug for the ipo proposal voting
    
    * qcapSoldAmount updated
    
    * fixes
    
    * fix: updated the Qvault V2 with the updated qpi
    
    ---------
    
    Co-authored-by: Philipp Werner <22914157+philippwerner@users.noreply.github.com>
    Co-authored-by: baoLuck <vitalymayer1706@gmail.com>

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -349,7 +349,7 @@ constexpr struct ContractDescription
     {"SWATCH", 123, 10000, sizeof(IPO)},
     {"CCF", 127, 10000, sizeof(CCF::StateData)}, // proposal in epoch 125, IPO in 126, construction and first use in 127
     {"QEARN", 137, 10000, sizeof(QEARN::StateData)}, // proposal in epoch 135, IPO in 136, construction in 137 / first donation after END_EPOCH, first round in epoch 138
-    {"QVAULT", 138, 10000, sizeof(IPO)}, // proposal in epoch 136, IPO in 137, construction and first use in 138
+    {"QVAULT", 138, 10000, sizeof(QVAULT::StateData)}, // proposal in epoch 136, IPO in 137, construction and first use in 138
     {"MSVAULT", 149, 10000, sizeof(MSVAULT::StateData)}, // proposal in epoch 147, IPO in 148, construction and first use in 149
     {"QBAY", 154, 10000, sizeof(QBAY::StateData)}, // proposal in epoch 152, IPO in 153, construction and first use in 154
     {"QSWAP", 171, 10000, sizeof(QSWAP::StateData)}, // proposal in epoch 169, IPO in 170, construction and first use in 171
