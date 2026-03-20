2eac0c1f8e05b800955499de1496d766473c1237
Ian Rachman Dana <rahmandana08@gmail.com>
2026-03-20 23:23:53 +0700

    feat: Add QSurv contract and tests (#771)
    * feat: Add QSurv trustless survey contract and tests
    
    - Add QSurv.h with StateData architecture (state.get/state.mut)
    - Register QSURV at index 25 in contract_def.h
    - Add contract_qsurv.cpp test suite
    - Add project references in vcxproj files
    
    * Adjust construction epoch for QSurv

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -264,6 +264,17 @@
 #define CONTRACT_STATE2_TYPE VOTTUNBRIDGE2
 #include "contracts/VottunBridge.h"
 
+#undef CONTRACT_INDEX
+#undef CONTRACT_STATE_TYPE
+#undef CONTRACT_STATE2_TYPE
+
+#define QSURV_CONTRACT_INDEX 26
+#define CONTRACT_INDEX QSURV_CONTRACT_INDEX
+#define CONTRACT_STATE_TYPE QSURV
+#define CONTRACT_STATE2_TYPE QSURV2
+#include "contracts/QSurv.h"
+
+
 
 // new contracts should be added above this line
 
@@ -376,6 +387,8 @@ constexpr struct ContractDescription
     {"QDUEL", 199, 10000, sizeof(QDUEL::StateData)}, // proposal in epoch 197, IPO in 198, construction and first use in 199
 	{"PULSE", 204, 10000, sizeof(PULSE::StateData)}, // proposal in epoch 202, IPO in 203, construction and first use in 204
     {"VOTTUN", 206, 10000, sizeof(VOTTUNBRIDGE::StateData)}, // proposal in epoch 204, IPO in 205, construction and first use in 206
+	{"QSURV", 207, 10000, sizeof(QSURV::StateData)}, // proposal in epoch 203, IPO in 204, construction and first use in 205
+
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     {"TESTEXA", 138, 10000, sizeof(TESTEXA::StateData)},
@@ -497,6 +510,7 @@ static void initializeContracts()
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QDUEL);
 	REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(PULSE);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(VOTTUNBRIDGE);
+	REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QSURV);
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(TESTEXA);
