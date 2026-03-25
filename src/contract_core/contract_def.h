110b913ba75bcaf378905a8586355307f06e282c
fnordspace <qubic@fnord.space>
2026-03-25 16:07:59 +0100

    Remove QSurv

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -264,20 +264,6 @@
 #define CONTRACT_STATE2_TYPE VOTTUNBRIDGE2
 #include "contracts/VottunBridge.h"
 
-#ifndef NO_QSURV
-
-#undef CONTRACT_INDEX
-#undef CONTRACT_STATE_TYPE
-#undef CONTRACT_STATE2_TYPE
-
-#define QSURV_CONTRACT_INDEX 26
-#define CONTRACT_INDEX QSURV_CONTRACT_INDEX
-#define CONTRACT_STATE_TYPE QSURV
-#define CONTRACT_STATE2_TYPE QSURV2
-#include "contracts/QSurv.h"
-
-#endif
-
 // new contracts should be added above this line
 
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
@@ -389,9 +375,6 @@ constexpr struct ContractDescription
     {"QDUEL", 199, 10000, sizeof(QDUEL::StateData)}, // proposal in epoch 197, IPO in 198, construction and first use in 199
 	{"PULSE", 204, 10000, sizeof(PULSE::StateData)}, // proposal in epoch 202, IPO in 203, construction and first use in 204
     {"VOTTUN", 206, 10000, sizeof(VOTTUNBRIDGE::StateData)}, // proposal in epoch 204, IPO in 205, construction and first use in 206
-#ifndef NO_QSURV
-	{"QSURV", 207, 10000, sizeof(QSURV::StateData)}, // proposal in epoch 205, IPO in 206, construction and first use in 207
-#endif
 
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
@@ -514,9 +497,6 @@ static void initializeContracts()
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QDUEL);
 	REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(PULSE);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(VOTTUNBRIDGE);
-#ifndef NO_QSURV
-	REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QSURV);
-#endif
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(TESTEXA);
