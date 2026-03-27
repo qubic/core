25827cd6d7eb9e90c649dd71a1ca5d7f940dd6ee
TakaYuPP <yutotaka24@gmail.com>
2026-03-27 12:22:12 +0100

    Added the Qusino smart contract (#762)
    * feat: first version of Qusino sc
    
    * feat: reimplementing the asset management and adding some of gtest cases
    
    * more testcases
    
    * feat: added more testcases
    
    * buyQST update with min pricing on the qx contract
    
    * fix: prefix issue
    
    * fix: rev1
    
    * fix: fixed the type of input for the asset&staking duration
    
    * fix: updated the qusino sc with updating the logic&added logs
    
    * fix: / to div
    
    * fix: daily bonus amount
    
    * fix: epoch for the first usage
    
    * fix: updated the redemption logic
    
    * removed unnecessary commit
    
    * unnecessary changes
    
    * fixed contract_def.h
    
    * updated the sc after updating the qpi
    
    * fixes
    
    * remove googletest-src
    
    * changed release epoch
    
    * protected helper functions

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -264,6 +264,16 @@
 #define CONTRACT_STATE2_TYPE VOTTUNBRIDGE2
 #include "contracts/VottunBridge.h"
 
+#undef CONTRACT_INDEX
+#undef CONTRACT_STATE_TYPE
+#undef CONTRACT_STATE2_TYPE
+
+#define QUSINO_CONTRACT_INDEX 26
+#define CONTRACT_INDEX QUSINO_CONTRACT_INDEX
+#define CONTRACT_STATE_TYPE QUSINO
+#define CONTRACT_STATE2_TYPE QUSINO2
+#include "contracts/Qusino.h"
+
 // new contracts should be added above this line
 
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
@@ -375,6 +385,7 @@ constexpr struct ContractDescription
     {"QDUEL", 199, 10000, sizeof(QDUEL::StateData)}, // proposal in epoch 197, IPO in 198, construction and first use in 199
 	{"PULSE", 204, 10000, sizeof(PULSE::StateData)}, // proposal in epoch 202, IPO in 203, construction and first use in 204
     {"VOTTUN", 206, 10000, sizeof(VOTTUNBRIDGE::StateData)}, // proposal in epoch 204, IPO in 205, construction and first use in 206
+  {"QUSINO", 208, 10000, sizeof(QUSINO::StateData)}, // proposal in epoch 206, IPO in 207, construction and first use in 208
 
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
@@ -497,6 +508,7 @@ static void initializeContracts()
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QDUEL);
 	REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(PULSE);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(VOTTUNBRIDGE);
+  REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QUSINO);
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(TESTEXA);
