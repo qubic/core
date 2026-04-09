c573a57a1466f70e57fdc6b63d20b701ce5dea4d
Franziska Mueller <11660876+Franziska-Mueller@users.noreply.github.com>
2026-04-09 20:19:09 +0200

    add additional data to doge oracle query

--- src/oracle_interfaces/DogeShareValidation.h
+++ src/oracle_interfaces/DogeShareValidation.h
@@ -1,3 +1,8 @@
+#pragma once
+
+#include "network_messages/common_def.h"
+#include "oracle_core/oracle_transactions.h"
+
 using namespace QPI;
 
 /**
@@ -13,19 +18,41 @@ struct DogeShareValidation
     /// Oracle query data / input to the oracle machine
     struct OracleQuery
     {
-        Array<uint8, 4> taskPartialHeaderVersion;
-        Array<uint8, 32> taskPartialHeaderPrevBlockHash;
-        Array<uint8, 32> solutionMerkleRoot;
-        Array<uint8, 4> solutionTime;
-        Array<uint8, 4> taskPartialHeaderDifficultyNBits;
-        Array<uint8, 4> solutionNonce;
         Array<uint8, 32> target;
+
+        Array<uint8, 4> solutionTime;
+        Array<uint8, 4> solutionNonce;
+        Array<uint8, 8> solutionExtraNonce2;
+        
+        Array<uint8, 4> taskPartialHeaderVersion;
+        Array<uint8, 4> taskPartialHeaderDifficultyNBits;
+        Array<uint8, 32> taskPartialHeaderPrevBlockHash;
+        uint32 extraNonce1NumBytes;
+        uint32 coinbase1NumBytes;
+        uint32 coinbase2NumBytes;
+        uint32 numMerkleBranches;
+
+        static constexpr uint64 additionalDataSize = MAX_INPUT_SIZE
+            - (32 + 4 + 4 + 8 + 4 + 4 + 32 + 4 * sizeof(uint32)) // size of all previous struct members
+            - OracleUserQueryTransactionPrefix::minInputSize();
+
+        uint8 additionalData[additionalDataSize];
+        // Layout for additional data:
+        // - extraNonce1
+        // - coinbase1
+        // - coinbase2
+        // - merkleBranch1NumBytes (unsigned int), ... , merkleBranchNNumBytes (unsigned int)
+        // - merkleBranch1, ... , merkleBranchN
+        // Note: The size of the components contained in the additional data varies, hence the total occupied bytes in the array is not fixed.
     };
+
+    static_assert(sizeof(OracleQuery) + OracleUserQueryTransactionPrefix::minInputSize() == MAX_INPUT_SIZE, "DogeShareValidation::OracleQuery has wrong size");
 
     /// Oracle reply data / output of the oracle machine
     struct OracleReply
     {
         bit isValid;
+        uint32 compIndex;
     };
 
     /// Return query fee, which may depend on the specific query (for example on the oracle).
