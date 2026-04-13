398ad44c1fca2525dad9aa328b326798f74833e2
baoLuck <91096117+baoLuck@users.noreply.github.com>
2026-04-13 17:52:28 +0300

    Escrow sc (#821)
    * Escrow sc
    
    * review fix
    
    ---------
    
    Co-authored-by: fnordspace <qubic@fnord.space>

--- src/contract_core/contract_def.h
+++ src/contract_core/contract_def.h
@@ -274,6 +274,19 @@
 #define CONTRACT_STATE2_TYPE QUSINO2
 #include "contracts/Qusino.h"
 
+#ifndef NO_ESCROW
+#undef CONTRACT_INDEX
+#undef CONTRACT_STATE_TYPE
+#undef CONTRACT_STATE2_TYPE
+
+#define ESCROW_CONTRACT_INDEX 27
+#define CONTRACT_INDEX ESCROW_CONTRACT_INDEX
+#define CONTRACT_STATE_TYPE ESCROW
+#define CONTRACT_STATE2_TYPE ESCROW2
+#include "contracts/Escrow.h"
+
+#endif
+
 // new contracts should be added above this line
 
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
@@ -386,6 +399,9 @@ constexpr struct ContractDescription
 	{"PULSE", 204, 10000, sizeof(PULSE::StateData)}, // proposal in epoch 202, IPO in 203, construction and first use in 204
     {"VOTTUN", 206, 10000, sizeof(VOTTUNBRIDGE::StateData)}, // proposal in epoch 204, IPO in 205, construction and first use in 206
     {"QUSINO", 208, 10000, sizeof(QUSINO::StateData)}, // proposal in epoch 206, IPO in 207, construction and first use in 208
+#ifndef NO_ESCROW
+    {"ESCROW", 210, 10000, sizeof(ESCROW::StateData)}, // proposal in epoch 208, IPO in 209, construction and first use in 210
+#endif
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     {"TESTEXA", 138, 10000, sizeof(TESTEXA::StateData)},
@@ -508,6 +524,9 @@ static void initializeContracts()
 	REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(PULSE);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(VOTTUNBRIDGE);
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(QUSINO);
+#ifndef NO_ESCROW
+    REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(ESCROW);
+#endif
     // new contracts should be added above this line
 #ifdef INCLUDE_CONTRACT_TEST_EXAMPLES
     REGISTER_CONTRACT_FUNCTIONS_AND_PROCEDURES(TESTEXA);
