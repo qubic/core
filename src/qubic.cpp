ee2c37840ba801833c5add6cc950e02f6a80ea4c
cyber-pc <cyber-pc@blue-pc.localhost>
2026-05-19 04:49:29 +0700

    add OLD_QSWAP toggle

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -3,6 +3,7 @@
 //#define INCLUDE_CONTRACT_TEST_EXAMPLES
 
 // #define OLD_QRAFFLE
+// #define OLD_QSWAP
 
 // contract_def.h needs to be included first to make sure that contracts have minimal access
 #include "contract_core/contract_def.h"
