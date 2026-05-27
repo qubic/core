1653728f56246f160297001715bc89171190ecdd
fnordspace <qubic@fnord.space>
2026-05-27 12:02:47 +0200

    Security patch of Qswap_old.h

--- src/contracts/Qswap_old.h
+++ src/contracts/Qswap_old.h
@@ -1356,7 +1356,7 @@ protected:
 		}
 
 		// check the vadility of input params
-		if (input.quAmountMin < 0 || input.assetAmountMin < 0)
+		if (input.quAmountMin < 0 || input.assetAmountMin < 0 || input.burnLiquidity <= 0)
 		{
 			return;
 		}
