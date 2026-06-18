233cfc42e65d8c29f03f95398a9ba7dcdf086c40
cyber-pc <165458555+cyber-pc@users.noreply.github.com>
2026-06-18 11:39:23 +0700

    Test setting for save load and vm

--- src/private_settings.h
+++ src/private_settings.h
@@ -33,12 +33,12 @@ static const unsigned char oracleMachineIPs[][4] = {
      {127, 0, 0, 1}, // REMOVE THIS ENTRY AND REPLACE IT WITH YOUR OWN IP ADDRESSES
 };
 
-#define ENABLE_QUBIC_LOGGING_EVENT 0 // turn on logging events
+#define ENABLE_QUBIC_LOGGING_EVENT 1 // turn on logging events
 
 // Virtual memory settings for logging
-#define LOG_BUFFER_PAGE_SIZE 300000000ULL
-#define PMAP_LOG_PAGE_SIZE 30000000ULL
-#define IMAP_LOG_PAGE_SIZE 10000ULL
+#define LOG_BUFFER_PAGE_SIZE 16384ULL //300000000ULL
+#define PMAP_LOG_PAGE_SIZE 1000ULL // 30000000ULL
+#define IMAP_LOG_PAGE_SIZE 10ULL //10000ULL
 #define VM_NUM_CACHE_PAGE 8
 
 #if ENABLE_QUBIC_LOGGING_EVENT
@@ -70,10 +70,10 @@ static unsigned long long logReaderPasscodes[4] = {
 // 0: disable
 // 1: save tick storage every TICK_STORAGE_AUTOSAVE_TICK_PERIOD ticks, only AUX mode
 // 2: save tick storage only when pressing the `F8` key or it is requested remotely
-#define TICK_STORAGE_AUTOSAVE_MODE 0
+#define TICK_STORAGE_AUTOSAVE_MODE 2
 // NOTE: Strategy to pick TICK_STORAGE_AUTOSAVE_TICK_PERIOD:
 // Although the default value is 1000, there is a chance that your node can be misaligned at tick XXXX2000,XXXX3000,XXXX4000,... 
 // Perform state persisting when your node is misaligned will also make your node misaligned after resuming.
 // Thus, picking various TICK_STORAGE_AUTOSAVE_TICK_PERIOD numbers across AUX nodes is recommended.
 // some suggested prime numbers you can try: 971 977 983 991 997
-#define TICK_STORAGE_AUTOSAVE_TICK_PERIOD 1337
+#define TICK_STORAGE_AUTOSAVE_TICK_PERIOD 50
