cbda123292df74c21a24b7e57cac65357b5aa5d1
fnordspace <qubic@fnord.space>
2026-06-22 17:23:30 +0200

    Remove OLD_QRAFFLE toggle

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -2,8 +2,6 @@
 
 // #define INCLUDE_CONTRACT_TEST_EXAMPLES
 
-// #define OLD_QRAFFLE
-
 // #define OLD_QBAY
 
 // contract_def.h needs to be included first to make sure that contracts have minimal access
