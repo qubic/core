8566e2a36fcc4c04bbbe80e846ef7f4a1789b901
cyber-pc <165458555+cyber-pc@users.noreply.github.com>
2026-06-29 14:54:16 +0700

    Revert "Add OLD_QBAY toggle to gate Qbay burn change"
    This reverts commit 76659607ea2c7fe4e98ff2431b35f330b1d29202.

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -2,7 +2,6 @@
 
 // #define INCLUDE_CONTRACT_TEST_EXAMPLES
 
-// #define OLD_QBAY
 
 // contract_def.h needs to be included first to make sure that contracts have minimal access
 #include "contract_core/contract_def.h"
