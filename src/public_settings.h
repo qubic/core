9ab4561cd109c297b7518eceb661cd70b653b51c
fnordspace <qubic@fnord.space>
2026-08-19 14:05:55 +0200

    update params for epoch 227 / v1.301.7

--- src/public_settings.h
+++ src/public_settings.h
@@ -71,11 +71,11 @@ static_assert(AUTO_FORCE_NEXT_TICK_THRESHOLD* TARGET_TICK_DURATION >= PEER_REFRE
 
 #define VERSION_A 1
 #define VERSION_B 301
-#define VERSION_C 6
+#define VERSION_C 7
 
 // Epoch and initial tick for node startup
-#define EPOCH 226
-#define TICK 73857162
+#define EPOCH 227
+#define TICK 75241781
 #define TICK_IS_FIRST_TICK_OF_EPOCH 1 // Set to 0 if the network is restarted during the EPOCH with a new initial TICK
 
 #define ARBITRATOR "AFZPUAIYVPNUYGJRQVLUKOPPVLHAZQTGLYAAUUNBXFTVTAMSBKQBLEIEPCVJ"
