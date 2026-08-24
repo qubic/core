a85db37aa3a020a6c1af9a1905b4a7a961da28f0
cyber-pc <165458555+cyber-pc@users.noreply.github.com>
2026-08-24 15:36:24 +0700

    Add OLD_QRAFFLE toggle.

--- src/qubic.cpp
+++ src/qubic.cpp
@@ -2,6 +2,7 @@
 
 // #define INCLUDE_CONTRACT_TEST_EXAMPLES
 
+// #define OLD_QRAFFLE
 
 // contract_def.h needs to be included first to make sure that contracts have minimal access
 #include "contract_core/contract_def.h"
